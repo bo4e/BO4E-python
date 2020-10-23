60b9922c2c9fe15224fbe1877c4fe4b3d7e9ad50
Kevin <kevin.krechan@hochfrequenz.de>
2020-10-23 21:23:03 +0200

    Replace too long line by multiline string

--- tests/test_adresse.py
+++ tests/test_adresse.py
@@ -14,9 +14,10 @@ class AdressTest(unittest.TestCase):
         )
         address_json = a.to_json()
         self.assertTrue('"DE"' in address_json, "Landescode did not default to 'DE'")
-        json_string = (
-            r'{"strasse":"Getreidegasse", "hausnummer":"9", "ort":"Salzburg", "postleitzahl":"5020", '
-            r'"landescode":"AT"} '
-        )
+        json_string = r"""{"strasse":"Getreidegasse",
+                 "hausnummer":"9",
+                 "ort":"Salzburg",
+                 "postleitzahl":"5020",
+                 "landescode":"AT"} """
         b: Adresse = Adresse.from_json(json_string)
         self.assertEqual(b.landescode, Landescode("Austria"))
