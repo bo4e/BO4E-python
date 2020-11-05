92cad7fe91ba8b2072dd6c0ddfff6c9959c9ad9c
Konstantin KLein <konstantin.klein@hochfrequenz.de>
2020-11-05 18:28:34 +0100

    test that the malo has required fields

--- tests/test_marktlokation.py
+++ tests/test_marktlokation.py
@@ -31,6 +31,14 @@ class MaLoTest(unittest.TestCase):
         self.assertEqual(malo.marktlokations_id, deserialized_malo.marktlokations_id)
         self.assertFalse(malo.marktlokations_id is deserialized_malo.marktlokations_id)
 
+    def test_malo_must_not_be_initialized_without_required_fields(self):
+        """
+        Business Objects need to be instantiated with all required values.
+        :return:
+        """
+        with self.assertRaises(TypeError):
+            _ = Marktlokation()
+
 
 if __name__ == "__main__":
     unittest.main()
