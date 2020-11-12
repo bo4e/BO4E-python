2dfd3b4eea54f49a0703afd28beb302286937685
Kevin <kevin.krechan@hochfrequenz.de>
2020-11-12 17:54:45 +0100

    Use attr instead of dataclass for adresse.py

--- src/bo4e/com/adresse.py
+++ src/bo4e/com/adresse.py
@@ -1,40 +1,28 @@
-from dataclasses import dataclass, field
-
-from dataclasses_json import dataclass_json, config
+import attr
+import jsons
 
 from bo4e.com.com import COM
 from bo4e.enum.landescode import Landescode
 
 
-@dataclass
-class _AdresseDefaultBase:
-    """
-    holds default values for Adresse
-    """
-
-    postfach: str = None
-    adresszusatz: str = None
-    co_ergaenzung: str = None
-    landescode: Landescode = field(
-        default="DE", metadata=config(encoder=Landescode.json_encoder)
-    )
-
-
-@dataclass
-class _AdresseBase:
-    """
-    holds those values that do not have a default value/are obligatory
-    """
-
-    postleitzahl: str
-    ort: str
-    strasse: str
-    hausnummer: str
-
-
-@dataclass_json
-@dataclass
-class Adresse(COM, _AdresseDefaultBase, _AdresseBase):
+@attr.s(auto_attribs=True)
+class Adresse(COM, jsons.JsonSerializable):
     """
     Enthält eine Adresse, die für die meisten Zwecke verwendbar ist.
     """
+
+    # required attributes
+    strasse: str
+    hausnummer: str
+    postleitzahl: str
+    ort: str
+
+    # optional attributes
+    postfach: str = attr.ib(init=False)
+    adresszusatz: str = attr.ib(init=False)
+    co_ergaenzung: str = attr.ib(init=False)
+    landescode: Landescode = attr.ib(default="DE")
+
+    # define de-/serialization for class Landescode
+    jsons.set_serializer(Landescode.json_encoder, Landescode)
+    jsons.set_deserializer(Landescode.json_decoder, Landescode)
