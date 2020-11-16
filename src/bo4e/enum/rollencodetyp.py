3a47d656224e8f36504dfcf3a5fbeec26bcc0088
Kevin <kevin.krechan@hochfrequenz.de>
2020-11-16 13:02:24 +0100

    Add docstring to class rollencodetyp

--- src/bo4e/enum/rollencodetyp.py
+++ src/bo4e/enum/rollencodetyp.py
@@ -2,6 +2,10 @@ from enum import Enum
 
 
 class Rollencodetyp(str, Enum):
+    """
+    Gibt den Codetyp einer Rolle, beispielsweise einer Marktrolle, an.
+    """
+
     BDEW = "BDEW"  # Bundesverband der Energie- u. Wasserwirtschaft
     DVG = "DVG"  # Deutscher Verein des Gas- und Wasserfaches
     GLN = "GLN"  # Global Location Number
