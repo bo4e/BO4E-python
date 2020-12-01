1568920fc148fe1aad2c82275e5e0a348b66d8f5
hf-aschloegl <73470827+hf-aschloegl@users.noreply.github.com>
2020-12-01 20:34:28 +0100

    Dictonary Comprehension
    Co-authored-by: hf-kklein <konstantin.klein@hochfrequenz.de>

--- src/bo4e/enum/landescode.py
+++ src/bo4e/enum/landescode.py
@@ -5,8 +5,6 @@ from iso3166 import countries
 Der ISO-Landescode als Enumeration.
 """
 
-alpha2codes = {}
-for c in countries:
-    alpha2codes[c.alpha2] = c.alpha2
+alpha2codes = {c.alpha2: c.alpha2 for c in countries}
 
 Landescode = Enum("Landescode", alpha2codes)
