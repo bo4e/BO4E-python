cf084792d891261e4cf6f8c003c3023c7b99879c
Annika Schlögl <annika.schloegl@hochfrequenz.de>
2020-12-07 16:30:18 +0100

    Add postfach as alternative to strasse
    Before strasse and hausnummer were required attributes and postfach was optional.  Since postfach should be an alternative address to strasse and hausnummer this was changed to either strasse and hausnummer or postfach are required attributes.
    Additionally the other required attributes are made sure to be strings and not empty.

--- src/bo4e/com/adresse.py
+++ src/bo4e/com/adresse.py
@@ -5,6 +5,17 @@ from bo4e.com.com import COM
 from bo4e.enum.landescode import Landescode
 
 
+def strasse_xor_postfach(instance, attribute, value):
+    if (
+        instance.strasse is None or instance.hausnummer is None
+    ) and instance.postfach is None:
+        raise ValueError("Either strasse and hausnummer or postfach are required.")
+    elif (
+        instance.strasse is not None or instance.hausnummer is not None
+    ) and instance.postfach is not None:
+        raise ValueError("Only strasse and hausnummer or postfach are required.")
+
+
 @attr.s(auto_attribs=True, kw_only=True)
 class Adresse(COM, jsons.JsonSerializable):
     """
@@ -12,13 +23,13 @@ class Adresse(COM, jsons.JsonSerializable):
     """
 
     # required attributes
-    strasse: str
-    hausnummer: str
-    postleitzahl: str
-    ort: str
+    strasse: str = attr.ib(default=None, validator=strasse_xor_postfach)
+    hausnummer: str = attr.ib(default=None, validator=strasse_xor_postfach)
+    postfach: str = attr.ib(default=None, validator=strasse_xor_postfach)
+    postleitzahl: str = attr.ib(validator=attr.validators.instance_of(str))
+    ort: str = attr.ib(validator=attr.validators.instance_of(str))
 
     # optional attributes
-    postfach: str = attr.ib(default=None)
     adresszusatz: str = attr.ib(default=None)
     co_ergaenzung: str = attr.ib(default=None)
     landescode: Landescode = attr.ib(default=Landescode.DE)
