319c48ecf4b5deebba23799354c89e83d94a7b2e
Annika Schlögl <annika.schloegl@hochfrequenz.de>
2020-12-07 16:34:45 +0100

    ✅ Add tests for postfach as required field
    Add two test: one for postfach as regular required field, the second for different combinations of strasse, hausnummer and postfach to test the validator in the Adresse class (adresse.py)

--- tests/test_adresse.py
+++ tests/test_adresse.py
@@ -7,7 +7,7 @@ from bo4e.enum.landescode import Landescode
 
 
 class TestAddress:
-    def test_serialization_only_required_fields(self):
+    def test_serialization_only_required_fields_strasse(self):
         with open(
             "./tests/test_data/test_data_adresse/test_data_adresse_only_required_fields.json",
             encoding="utf-8",
@@ -28,6 +28,34 @@ class TestAddress:
         )
 
         assert "DE" in address_json
+
+    def test_serialization_only_required_fields_postfach(self):
+        address_test_data = {
+            "postleitzahl": "82031",
+            "ort": "Grünwald",
+            "postfach": "10 64 38",
+        }
+
+        a = Adresse(
+            postleitzahl=address_test_data["postleitzahl"],
+            ort=address_test_data["ort"],
+            postfach=address_test_data["postfach"],
+        )
+
+        address_json = a.dumps(
+            strip_nulls=True,
+            key_transformer=jsons.KEY_TRANSFORMER_CAMELCASE,
+            jdkwargs={"ensure_ascii": False},
+        )
+
+        assert "10 64 38" in address_json
+        assert "82031" in address_json
+
+        deserialized_address = Adresse.loads(address_json)
+
+        assert isinstance(deserialized_address, Adresse)
+        assert deserialized_address.postfach == "10 64 38"
+        assert deserialized_address.postleitzahl == "82031"
 
     def test_serialization_only_required_fields_landescode_AT(self):
         with open(
@@ -82,3 +110,65 @@ class TestAddress:
             )
 
         assert "postleitzahl" in str(excinfo.value)
+
+    @pytest.mark.parametrize(
+        "address_test_data, expected",
+        [
+            pytest.param(
+                {
+                    "postleitzahl": "82031",
+                    "ort": "Grünwald",
+                    "strasse": None,
+                    "hausnummer": "27A",
+                    "postfach": None,
+                },
+                "Either",
+                id="Missing strasse",
+            ),
+            pytest.param(
+                {
+                    "postleitzahl": "82031",
+                    "ort": "Grünwald",
+                    "strasse": "Nördliche Münchner Straße",
+                    "hausnummer": None,
+                    "postfach": None,
+                },
+                "Either",
+                id="Missing hausnummer",
+            ),
+            pytest.param(
+                {
+                    "postleitzahl": "82031",
+                    "ort": "Grünwald",
+                    "strasse": None,
+                    "hausnummer": None,
+                    "postfach": None,
+                },
+                "Either",
+                id="Missing postfach",
+            ),
+            pytest.param(
+                {
+                    "postleitzahl": "82031",
+                    "ort": "Grünwald",
+                    "strasse": None,
+                    "hausnummer": "27A",
+                    "postfach": "10 64 38",
+                },
+                "Only",
+                id="Postfach and hausnummer",
+            ),
+        ],
+    )
+    def test_strasse_xor_postfach(self, address_test_data, expected):
+
+        with pytest.raises(ValueError) as excinfo:
+
+            _ = Adresse(
+                postleitzahl=address_test_data["postleitzahl"],
+                ort=address_test_data["ort"],
+                strasse=address_test_data["strasse"],
+                hausnummer=address_test_data["hausnummer"],
+                postfach=address_test_data["postfach"],
+            )
+        assert expected in str(excinfo.value)
