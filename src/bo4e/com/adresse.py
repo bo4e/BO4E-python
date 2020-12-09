ee2a3fa9eb2f4daeb6f82b82fc8c3959488f86e0
Annika Schlögl <annika.schloegl@hochfrequenz.de>
2020-12-09 08:50:39 +0100

    Update address requirements
    The minimal address in Germany is only a zip code and city  (for example for institutions like universities or big companies or postfächer without a number but a special zip code) 
    A street always needs a hausnummer and vice versa.
    Postfächer don't have a street, but a number OR a zipcode.

--- src/bo4e/com/adresse.py
+++ src/bo4e/com/adresse.py
@@ -6,14 +6,13 @@ from bo4e.enum.landescode import Landescode
 
 
 def strasse_xor_postfach(instance, attribute, value):
-    if (
-        instance.strasse is None or instance.hausnummer is None
-    ) and instance.postfach is None:
-        raise ValueError("Either strasse and hausnummer or postfach are required.")
-    elif (
-        instance.strasse is not None or instance.hausnummer is not None
-    ) and instance.postfach is not None:
-        raise ValueError("Only strasse and hausnummer or postfach are required.")
+    if instance.strasse or instance.hausnummer:
+        if instance.postfach:
+            raise ValueError("Enter either strasse and hausnummer OR postfach.")
+        elif not instance.strasse:
+            raise ValueError("Missing strasse to hausnummer.")
+        elif not instance.hausnummer:
+            raise ValueError("Missing hausnummer to strasse.")
 
 
 @attr.s(auto_attribs=True, kw_only=True)
@@ -23,13 +22,13 @@ class Adresse(COM, jsons.JsonSerializable):
     """
 
     # required attributes
-    strasse: str = attr.ib(default=None, validator=strasse_xor_postfach)
-    hausnummer: str = attr.ib(default=None, validator=strasse_xor_postfach)
-    postfach: str = attr.ib(default=None, validator=strasse_xor_postfach)
     postleitzahl: str = attr.ib(validator=attr.validators.instance_of(str))
     ort: str = attr.ib(validator=attr.validators.instance_of(str))
 
     # optional attributes
+    strasse: str = attr.ib(default=None, validator=strasse_xor_postfach)
+    hausnummer: str = attr.ib(default=None, validator=strasse_xor_postfach)
+    postfach: str = attr.ib(default=None, validator=strasse_xor_postfach)
     adresszusatz: str = attr.ib(default=None)
     co_ergaenzung: str = attr.ib(default=None)
     landescode: Landescode = attr.ib(default=Landescode.DE)
