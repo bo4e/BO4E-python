1d438a420c766948d7b8ae67d62e84bc5f76b4c4
Annika Schlögl <annika.schloegl@hochfrequenz.de>
2020-12-09 08:54:12 +0100

    ✅ Update tests for updated address requirements
    Added test for only postleitzahl and ort
    Added docstrings to serialization tests of strasse and postleitzahl
    Updated test for the function strasse_xor_postfach in Class Adresse (adresse.py) for the updates in the requirements of a german address

--- tests/test_adresse.py
+++ tests/test_adresse.py
@@ -6,7 +6,11 @@ from bo4e.enum.landescode import Landescode
 
 
 class TestAddress:
-    def test_serialization_only_required_fields_strasse(self):
+    def test_serialization_strasse(self):
+        """
+        Test serialization with strasse und hausnummer
+        and default value of landescode
+        """
         with open(
             "./tests/test_data/test_data_adresse/test_data_adresse_only_required_fields.json",
             encoding="utf-8",
@@ -50,23 +54,6 @@ class TestAddress:
             postfach=address_test_data["postfach"],
         )
 
-        assert "Nördliche Münchner Straße" in address_json
-        assert "27A" in address_json
-        assert "82031" in address_json
-
-    def test_serialization_only_required_fields_postfach(self):
-        address_test_data = {
-            "postleitzahl": "82031",
-            "ort": "Grünwald",
-            "postfach": "10 64 38",
-        }
-
-        a = Adresse(
-            postleitzahl=address_test_data["postleitzahl"],
-            ort=address_test_data["ort"],
-            postfach=address_test_data["postfach"],
-        )
-
         address_json = a.dumps(
             strip_nulls=True,
             key_transformer=jsons.KEY_TRANSFORMER_CAMELCASE,
@@ -83,6 +70,33 @@ class TestAddress:
         assert deserialized_address.postfach == "10 64 38"
         assert deserialized_address.postleitzahl == "82031"
         assert deserialized_address.landescode == Landescode.DE
+
+    def test_serialization_only_required_fields(self):
+        """Test serialization with just postleitzahl und ort"""
+        address_test_data = {
+            "postleitzahl": "82031",
+            "ort": "Grünwald",
+        }
+
+        a = Adresse(
+            postleitzahl=address_test_data["postleitzahl"],
+            ort=address_test_data["ort"],
+        )
+
+        address_json = a.dumps(
+            strip_nulls=True,
+            key_transformer=jsons.KEY_TRANSFORMER_CAMELCASE,
+            jdkwargs={"ensure_ascii": False},
+        )
+
+        assert "Grünwald" in address_json
+        assert "82031" in address_json
+
+        deserialized_address = Adresse.loads(address_json)
+
+        assert isinstance(deserialized_address, Adresse)
+        assert deserialized_address.ort == "Grünwald"
+        assert deserialized_address.postleitzahl == "82031"
 
     def test_serialization_only_required_fields_landescode_AT(self):
         with open(
@@ -145,11 +159,22 @@ class TestAddress:
                 {
                     "postleitzahl": "82031",
                     "ort": "Grünwald",
-                    "strasse": None,
+                    "strasse": "Nördliche Münchner Straße",
+                    "hausnummer": None,
+                    "postfach": None,
+                },
+                "Missing hausnummer",
+                id="Missing hausnummer",
+            ),
+            pytest.param(
+                {
+                    "postleitzahl": "82031",
+                    "ort": "Grünwald",
+                    "strasse": "",
                     "hausnummer": "27A",
                     "postfach": None,
                 },
-                "Either",
+                "Missing strasse",
                 id="Missing strasse",
             ),
             pytest.param(
@@ -157,22 +182,11 @@ class TestAddress:
                     "postleitzahl": "82031",
                     "ort": "Grünwald",
                     "strasse": "Nördliche Münchner Straße",
-                    "hausnummer": None,
-                    "postfach": None,
+                    "hausnummer": "27A",
+                    "postfach": "10 64 38",
                 },
-                "Either",
-                id="Missing hausnummer",
-            ),
-            pytest.param(
-                {
-                    "postleitzahl": "82031",
-                    "ort": "Grünwald",
-                    "strasse": None,
-                    "hausnummer": None,
-                    "postfach": None,
-                },
-                "Either",
-                id="Missing postfach",
+                "hausnummer OR postfach",
+                id="Strasse, hausnummer and postfach",
             ),
             pytest.param(
                 {
@@ -182,7 +196,7 @@ class TestAddress:
                     "hausnummer": "27A",
                     "postfach": "10 64 38",
                 },
-                "Only",
+                "hausnummer OR postfach",
                 id="Postfach and hausnummer",
             ),
         ],
