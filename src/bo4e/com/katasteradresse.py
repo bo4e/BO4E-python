fcbe5b204c0c41cbee72790dcff5c8c33b165090
Kevin <kevin.krechan@hochfrequenz.de>
2020-12-19 13:28:17 +0100

    ♻️ Replace jsons with marshmallow in katasteradresse

--- src/bo4e/com/katasteradresse.py
+++ src/bo4e/com/katasteradresse.py
@@ -1,14 +1,25 @@
 import attr
-import jsons
 
+from marshmallow import Schema, fields, post_load
+
+from bo4e.cases import JavaScriptMixin
 from bo4e.com.com import COM
 
 
 @attr.s(auto_attribs=True, kw_only=True)
-class Katasteradresse(COM, jsons.JsonSerializable):
+class Katasteradresse(COM):
     """
     Dient der Adressierung über die Liegenschafts-Information.
     """
 
     gemarkung_flur: str
     flurstueck: str
+
+
+class KatasteradresseSchema(Schema, JavaScriptMixin):
+    gemarkung_flur = fields.Str()
+    flurstueck = fields.Str()
+
+    @post_load
+    def make_katasteradresse(self, data, **kwargs) -> Katasteradresse:
+        return Katasteradresse(**data)
