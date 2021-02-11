04aa112abe10d4d22a1b3aa78014e1c8fdedc8e9
Kevin <kevin.krechan@hochfrequenz.de>
2021-02-11 09:54:15 +0100

    🚨 Add docstrings and disable two warnings
    warnings:
    * Unused argument 'kwargs' (unused-argument)
    * Method could be a function (no-self-use)

--- src/bo4e/cases.py
+++ src/bo4e/cases.py
@@ -1,17 +1,25 @@
+"""
+Small module to convert snake_case to CamelCase and vice versa.
+"""
+# pylint: disable=unused-argument, no-self-use
+
 from stringcase import camelcase, snakecase
 from marshmallow import pre_load, post_dump
 
 
 class JavaScriptMixin:
     """
-    This class is used so that we always use snake_case in the Python world and CamelCase in the JSON (Javascript world).
+    This class is used so that we always use snake_case in the Python world and
+    CamelCase in the JSON (Javascript world).
     Its methods are executed before loading or after dumping a JSON string respectively.
     """
 
     @pre_load
     def to_snakecase(self, data, **kwargs):
+        """ convert to snake_case """
         return {snakecase(key): value for key, value in data.items()}
 
     @post_dump
     def to_camelcase(self, data, **kwargs):
+        """ convert to CamelCase """
         return {camelcase(key): value for key, value in data.items()}
