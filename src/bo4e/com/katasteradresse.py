fe88d9bd161411c0f810e18f2ff1dbbec8cc08f7
Kevin <kevin.krechan@hochfrequenz.de>
2021-02-11 11:07:24 +0100

    🚨 Add docstrings and disable warnings
    * R0903: Too few public methods (0/2) (too-few-public-methods)
    * W0613: Unused argument 'kwargs' (unused-argument)
    * R0201: Method could be a function (no-self-use)

--- src/bo4e/com/katasteradresse.py
+++ src/bo4e/com/katasteradresse.py
@@ -1,10 +1,14 @@
+"""
+Contains Katasteradresse class
+and corresponding marshmallow schema for de-/serialization
+"""
 import attr
 from marshmallow import Schema, fields, post_load
 
 from bo4e.cases import JavaScriptMixin
 from bo4e.com.com import COM
 
-
+# pylint: disable=too-few-public-methods
 @attr.s(auto_attribs=True, kw_only=True)
 class Katasteradresse(COM):
     """
@@ -16,9 +20,16 @@ class Katasteradresse(COM):
 
 
 class KatasteradresseSchema(Schema, JavaScriptMixin):
+    """
+    Schema for de-/serialization of Katasteradresse.
+    Inherits from Schema and JavaScriptMixin.
+    """
+
     gemarkung_flur = fields.Str()
     flurstueck = fields.Str()
 
+    # pylint: disable=no-self-use, unused-argument
     @post_load
     def deserialise(self, data, **kwargs) -> Katasteradresse:
+        """ Deserialize JSON to Katasteradresse object """
         return Katasteradresse(**data)
