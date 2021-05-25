217abc64f590917977299fce385ae2157522b1ff
konstantin <konstantin.klein@hochfrequenz.de>
2021-05-25 17:18:34 +0200

    remove '"'

--- src/bo4e/enum/zaehlerauspraegung.py
+++ src/bo4e/enum/zaehlerauspraegung.py
@@ -5,7 +5,7 @@ Gibt an, ob es sich um einen Einrichtungs- oder Zweirichtungszähler handelt.
 from enum import Enum
 
 _zaehlerauspraegung = {
-    "EINRICHTUNGSZAEHLER": "EINRICHTUNGSZAEHLER",  # Einrichtungszaehler"
-    "ZWEIRICHTUNGSZAEHLER": "ZWEIRICHTUNGSZAEHLER",  # Zweirichtungszaehler"
+    "EINRICHTUNGSZAEHLER": "EINRICHTUNGSZAEHLER",  # Einrichtungszaehler
+    "ZWEIRICHTUNGSZAEHLER": "ZWEIRICHTUNGSZAEHLER",  # Zweirichtungszaehler
 }
 Zaehlerauspraegung = Enum("Zaehlerauspraegung", _zaehlerauspraegung)
--- src/bo4e/enum/zaehlertyp.py
+++ src/bo4e/enum/zaehlertyp.py
@@ -5,14 +5,14 @@ Bei diesem Enum handelt es sich die Abbildung von Zählertypen der Sparten Strom
 from enum import Enum
 
 _zaehlertyp = {
-    "DREHSTROMZAEHLER": "DREHSTROMZAEHLER",  # Drehstromzähler"
-    "BALGENGASZAEHLER": "BALGENGASZAEHLER",  # Balgengaszähler"
-    "DREHKOLBENZAEHLER": "DREHKOLBENZAEHLER",  # Drehkolbengaszähler"
-    "SMARTMETER": "SMARTMETER",  # Smart Meter Zähler"
-    "LEISTUNGSZAEHLER": "LEISTUNGSZAEHLER",  # leistungsmessender Zähler"
-    "MAXIMUMZAEHLER": "MAXIMUMZAEHLER",  # Maximumzähler"
-    "TURBINENRADGASZAEHLER": "TURBINENRADGASZAEHLER",  # Turbinenradgaszähler"
-    "ULTRASCHALLGASZAEHLER": "ULTRASCHALLGASZAEHLER",  # Ultraschallgaszähler"
-    "WECHSELSTROMZAEHLER": "WECHSELSTROMZAEHLER",  # Wechselstromzähler"
+    "DREHSTROMZAEHLER": "DREHSTROMZAEHLER",  # Drehstromzähler
+    "BALGENGASZAEHLER": "BALGENGASZAEHLER",  # Balgengaszähler
+    "DREHKOLBENZAEHLER": "DREHKOLBENZAEHLER",  # Drehkolbengaszähler
+    "SMARTMETER": "SMARTMETER",  # Smart Meter Zähler
+    "LEISTUNGSZAEHLER": "LEISTUNGSZAEHLER",  # leistungsmessender Zähler
+    "MAXIMUMZAEHLER": "MAXIMUMZAEHLER",  # Maximumzähler
+    "TURBINENRADGASZAEHLER": "TURBINENRADGASZAEHLER",  # Turbinenradgaszähler
+    "ULTRASCHALLGASZAEHLER": "ULTRASCHALLGASZAEHLER",  # Ultraschallgaszähler
+    "WECHSELSTROMZAEHLER": "WECHSELSTROMZAEHLER",  # Wechselstromzähler
 }
 Zaehlertyp = Enum("Zaehlertyp", _zaehlertyp)
