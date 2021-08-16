18ba0415882bdf66668ea701409ce54d02f1d799
Kevin <kevin.krechan@hochfrequenz.de>
2021-08-16 17:54:11 +0200

    🔥 Delete version info cause it seems to be the source of build error

--- docs/conf.py
+++ docs/conf.py
@@ -13,13 +13,6 @@ import os
 import shutil
 import sys
 
-# from pkg_resources import get_distribution
-# from importlib.metadata import version as get_version
-
-# Get version information for the package documentation
-# which is published on read the docs: https://bo4e-python.readthedocs.io/en/latest/
-# release = get_version("bo4e")
-# version = ".".join(release.split(".")[:2])
 
 __location__ = os.path.join(os.getcwd(), os.path.dirname(inspect.getfile(inspect.currentframe())))
 
