f3cad92901cb4f187d1c75a9e000416b636c1f93
konstantin <konstantin.klein@hochfrequenz.de>
2021-11-01 16:37:15 +0100

    there is no strenum in python 3.10

--- src/bo4e/enum/strenum.py
+++ src/bo4e/enum/strenum.py
@@ -2,16 +2,13 @@
 strenum contains an enum that inherits from the plain enum and string.
 """
 
-import sys
 from enum import Enum
 
-if sys.version_info < (3, 10):
-    # pylint: disable=too-few-public-methods
-    class StrEnum(str, Enum):
-        """
-        An enum with string values. This is obsolete for Python >=3.10
-        """
 
+# pylint: disable=too-few-public-methods
+class StrEnum(str, Enum):
+    """
+    An enum that has string values.
+    """
 
-else:
-    from enum import StrEnum  # pylint: disable=unused-import
+    # see https://docs.python.org/3/library/enum.html?highlight=strenum#others
