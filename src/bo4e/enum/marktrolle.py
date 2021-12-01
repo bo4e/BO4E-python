ed93e6cb6c66451041018447ecd98ee3dbb4fc0c
konstantin <konstantin.klein@hochfrequenz.de>
2021-12-01 10:25:59 +0100

    📝🎨update enum member documentation to sphinx compatible format (#81)

--- src/bo4e/enum/marktrolle.py
+++ src/bo4e/enum/marktrolle.py
@@ -8,17 +8,17 @@ class Marktrolle(StrEnum):
     Diese Rollen kann ein Marktteilnehmer einnehmen.
     """
 
-    NB = "NB"  # Grid Operator
-    LF = "LF"  # Supplier
-    MSB = "MSB"  # Messstellenbetreiber
-    DL = "DL"  # Dienstleister
-    BKV = "BKV"  # Bilanzkreisverantwortlicher,
-    BKO = "BKO"  # Bilanzkoordinator / Marktgebietsverantwortlicher
-    UENB = "UENB"  # Übertragungsnetzbetreiber
-    KUNDE_SELBST_NN = "KUNDE_SELBST_NN"  # Kunden, die Netznutzungsentgelte selbst zahlen
-    MGV = "MGV"  # Marktgebietsverantwortlicher
-    EIV = "EIV"  # Einsatzverantwortlicher
-    RB = "RB"  # Registerbetreiber
+    NB = "NB"  #: Grid Operator
+    LF = "LF"  #: Supplier
+    MSB = "MSB"  #: Messstellenbetreiber
+    DL = "DL"  #: Dienstleister
+    BKV = "BKV"  #: Bilanzkreisverantwortlicher,
+    BKO = "BKO"  #: Bilanzkoordinator / Marktgebietsverantwortlicher
+    UENB = "UENB"  #: Übertragungsnetzbetreiber
+    KUNDE_SELBST_NN = "KUNDE_SELBST_NN"  #: Kunden, die Netznutzungsentgelte selbst zahlen
+    MGV = "MGV"  #: Marktgebietsverantwortlicher
+    EIV = "EIV"  #: Einsatzverantwortlicher
+    RB = "RB"  #: Registerbetreiber
     KUNDE = "KUNDE"
     INTERESSENT = "INTERESSENT"
-    BTR = "BTR"  # Betreiber einer technischen Ressource
+    BTR = "BTR"  #: Betreiber einer technischen Ressource
--- src/bo4e/enum/zaehlertyp.py
+++ src/bo4e/enum/zaehlertyp.py
@@ -8,12 +8,12 @@ class Zaehlertyp(StrEnum):
     Bei diesem Enum handelt es sich um die Abbildung von Zählertypen der Sparten Strom und Gas.
     """
 
-    DREHSTROMZAEHLER = "DREHSTROMZAEHLER"  # Drehstromzähler
-    BALGENGASZAEHLER = "BALGENGASZAEHLER"  # Balgengaszähler
-    DREHKOLBENZAEHLER = "DREHKOLBENZAEHLER"  # Drehkolbengaszähler
-    SMARTMETER = "SMARTMETER"  # Smart Meter Zähler
-    LEISTUNGSZAEHLER = "LEISTUNGSZAEHLER"  # leistungsmessender Zähler
-    MAXIMUMZAEHLER = "MAXIMUMZAEHLER"  # Maximumzähler
-    TURBINENRADGASZAEHLER = "TURBINENRADGASZAEHLER"  # Turbinenradgaszähler
-    ULTRASCHALLGASZAEHLER = "ULTRASCHALLGASZAEHLER"  # Ultraschallgaszähler
-    WECHSELSTROMZAEHLER = "WECHSELSTROMZAEHLER"  # Wechselstromzähler
+    DREHSTROMZAEHLER = "DREHSTROMZAEHLER"  #: Drehstromzähler
+    BALGENGASZAEHLER = "BALGENGASZAEHLER"  #: Balgengaszähler
+    DREHKOLBENZAEHLER = "DREHKOLBENZAEHLER"  #: Drehkolbengaszähler
+    SMARTMETER = "SMARTMETER"  #: Smart Meter Zähler
+    LEISTUNGSZAEHLER = "LEISTUNGSZAEHLER"  #: leistungsmessender Zähler
+    MAXIMUMZAEHLER = "MAXIMUMZAEHLER"  #: Maximumzähler
+    TURBINENRADGASZAEHLER = "TURBINENRADGASZAEHLER"  #: Turbinenradgaszähler
+    ULTRASCHALLGASZAEHLER = "ULTRASCHALLGASZAEHLER"  #: Ultraschallgaszähler
+    WECHSELSTROMZAEHLER = "WECHSELSTROMZAEHLER"  #: Wechselstromzähler
