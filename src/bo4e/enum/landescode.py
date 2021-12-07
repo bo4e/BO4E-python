ce274d2cc961def63f2588433d931627264f0e49
konstantin <konstantin.klein@hochfrequenz.de>
2021-12-07 16:36:44 +0100

    remove ignore imports

--- src/bo4e/enum/landescode.py
+++ src/bo4e/enum/landescode.py
@@ -3,7 +3,7 @@ Der ISO-Landescode als Enumeration.
 """
 from enum import Enum
 
-from iso3166 import countries  # type: ignore
+from iso3166 import countries
 
 alpha2codes = {c.alpha2: c.alpha2 for c in countries}
 
