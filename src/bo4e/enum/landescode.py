921b14e4150f18cb31ae8b0267f2959a5b16a183
konstantin <konstantin.klein@hochfrequenz.de>
2021-12-07 16:38:58 +0100

    does this work?

--- src/bo4e/enum/landescode.py
+++ src/bo4e/enum/landescode.py
@@ -7,4 +7,4 @@ from iso3166 import countries
 
 alpha2codes = {c.alpha2: c.alpha2 for c in countries}
 
-Landescode = Enum("Landescode", alpha2codes)  # type: ignore
+Landescode = Enum("Landescode", alpha2codes)
