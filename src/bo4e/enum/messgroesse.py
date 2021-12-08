33ecadc5233086b776dafdd5197091c045dd3993
konstantin <konstantin.klein@hochfrequenz.de>
2021-12-08 10:33:17 +0100

    do docstrings consistently in Title case

--- src/bo4e/enum/messgroesse.py
+++ src/bo4e/enum/messgroesse.py
@@ -7,16 +7,16 @@ class Messgroesse(StrEnum):
     Gibt die physikalische Größe an, die gemessen wurde.
     """
 
-    STROM = "STROM"  #: STROM
-    SPANNUNG = "SPANNUNG"  #: SPANNUNG
-    WIRKLEISTUNG = "WIRKLEISTUNG"  #: WIRKLEISTUNG
-    BLINDLEISTUNG = "BLINDLEISTUNG"  #: BLINDLEISTUNG
-    DRUCK = "DRUCK"  #: DRUCK
-    LASTGANG = "LASTGANG"  #: LASTGANG
-    LASTPROFIL = "LASTPROFIL"  #: LASTPROFIL
-    TEMPERATUR = "TEMPERATUR"  #: TEMPERATUR
+    STROM = "STROM"  #: Strom
+    SPANNUNG = "SPANNUNG"  #: Spannung
+    WIRKLEISTUNG = "WIRKLEISTUNG"  #: Wirkleistung
+    BLINDLEISTUNG = "BLINDLEISTUNG"  #: Blindleistung
+    DRUCK = "DRUCK"  #: Druck
+    LASTGANG = "LASTGANG"  #: Lastgang
+    LASTPROFIL = "LASTPROFIL"  #: Lastprofil
+    TEMPERATUR = "TEMPERATUR"  #: Temperatur
     ZZAHL = "ZZAHL"  #: Zustandszahl
-    BRENNWERT = "BRENNWERT"  #: BRENNWERT
-    GRADTZAGSZAHLEN = "GRADTZAGSZAHLEN"  #: GRADTZAGSZAHLEN
-    VOLUMENSTROM = "VOLUMENSTROM"  #: VOLUMENSTROM
-    PREISE = "PREISE"  #: PREISE
+    BRENNWERT = "BRENNWERT"  #: Brennwert
+    GRADTZAGSZAHLEN = "GRADTZAGSZAHLEN"  #: Gradtagszahlen
+    VOLUMENSTROM = "VOLUMENSTROM"  #: Volumenstrom
+    PREISE = "PREISE"  #: Preise
