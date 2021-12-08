baf9cb51873478774550555a59cbb98792af9fb9
Kevin <kevin.krechan@hochfrequenz.de>
2021-12-08 14:06:27 +0100

    🎨 Add Marktgebietinfo class

---
+++ src/bo4e/com/marktgebietinfo.py
@@ -0,0 +1,34 @@
+"""
+Contains Marktgebietinfo class
+and corresponding marshmallow schema for de-/serialization
+"""
+import attr
+from marshmallow import fields, post_load
+
+from bo4e.com.com import COM, COMSchema
+
+
+# pylint: disable=too-few-public-methods
+@attr.s(auto_attribs=True, kw_only=True)
+class Marktgebietinfo(COM):
+    """
+    Informationen zum Marktgebiet im Gas.
+    """
+
+    marktgebiet: str
+    marktgebietcode: str
+
+
+class MarktgebietinfoSchema(COMSchema):
+    """
+    Schema for de-/serialization of Marktgebietinfo.
+    """
+
+    marktgebiet = fields.Str()
+    marktgebietcode = fields.Str()
+
+    # pylint: disable=no-self-use, unused-argument
+    @post_load
+    def deserialize(self, data, **kwargs) -> Marktgebietinfo:
+        """Deserialize JSON to Katasteradresse object"""
+        return Marktgebietinfo(**data)
