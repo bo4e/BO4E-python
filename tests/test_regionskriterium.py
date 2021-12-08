3055d56ca97e87c4ec2e0826ac4100f559c85f6c
konstantin <konstantin.klein@hochfrequenz.de>
2021-12-08 15:33:54 +0100

    ignore typing

--- tests/test_regionskriterium.py
+++ tests/test_regionskriterium.py
@@ -3,7 +3,7 @@ import pytest  # type:ignore[import]
 from bo4e.com.regionskriterium import Regionskriterium, RegionskriteriumSchema
 from bo4e.enum.gueltigkeitstyp import Gueltigkeitstyp
 from bo4e.enum.regionskriteriumtyp import Regionskriteriumtyp
-from tests.serialization_helper import assert_serialization_roundtrip
+from tests.serialization_helper import assert_serialization_roundtrip  # type:ignore[import]
 
 
 class TestRegionskriterium:
