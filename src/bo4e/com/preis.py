5171ab775953abf9b6b0236eb41ee5b1b870e4b7
Robert M <robert.morlock@hochfrequenz.de>
2021-12-08 15:29:56 +0100

    add Preis COM

---
+++ src/bo4e/com/preis.py
@@ -0,0 +1,52 @@
+"""
+Contains Preis class
+and corresponding marshmallow schema for de-/serialization
+"""
+
+from decimal import Decimal
+from typing import Optional
+
+import attr
+from marshmallow import fields, post_load
+from marshmallow_enum import EnumField  # type:ignore[import]
+
+from bo4e.com.com import COM, COMSchema
+from bo4e.enum.waehrungseinheit import Waehrungseinheit
+from bo4e.enum.mengeneinheit import Mengeneinheit
+from bo4e.enum.preisstatus import Preisstatus
+
+
+# pylint: disable=too-few-public-methods
+@attr.s(auto_attribs=True, kw_only=True)
+class Preis(COM):
+    """
+    Abbildung eines Preises mit Wert, Einheit, Bezugswert und Status.
+    """
+
+    # required attributes
+    wert: Decimal = attr.ib(validator=attr.validators.instance_of(Decimal))
+    einheit: Waehrungseinheit = attr.ib(validator=attr.validators.in_(Waehrungseinheit))
+    bezugswert: Mengeneinheit = attr.ib(validator=attr.validators.in_(Mengeneinheit))
+
+    # optional attributes
+    status: Optional[Preisstatus] = attr.ib(default=None, validator=attr.validators.in_(Preisstatus))
+
+
+class PreisSchema(COMSchema):
+    """
+    Schema for de-/serialization of Preis.
+    """
+
+    # required attributes
+    wert = fields.Decimal()
+    einheit = EnumField(Waehrungseinheit)
+    bezugswert = EnumField(Mengeneinheit)
+
+    # optional attributes
+    status = EnumField(Preisstatus, load_default=None)
+
+    # pylint: disable=no-self-use, unused-argument
+    @post_load
+    def deserialize(self, data, **kwargs) -> Preis:
+        """Deserialize JSON to Preis object"""
+        return Preis(**data)
