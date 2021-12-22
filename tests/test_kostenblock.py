1474e4477f3132323a711824f13726f66e57df44
konstantin <konstantin.klein@hochfrequenz.de>
2021-12-22 08:31:01 +0100

    fix tests

--- tests/test_kostenblock.py
+++ tests/test_kostenblock.py
@@ -6,7 +6,6 @@ from bo4e.com.betrag import Betrag
 from bo4e.com.kostenblock import Kostenblock, KostenblockSchema
 from bo4e.com.kostenposition import Kostenposition
 from bo4e.com.preis import Preis
-from bo4e.com.preisstaffel import Preisstaffel, PreisstaffelSchema
 from bo4e.enum.mengeneinheit import Mengeneinheit
 from bo4e.enum.preisstatus import Preisstatus
 from bo4e.enum.waehrungscode import Waehrungscode
@@ -58,25 +57,25 @@ class TestKostenblock:
                 ),
                 {
                     "kostenblockbezeichnung": "Mein Kostenblock",
-                    "summeKostenblock": {"wert": "12.5", "waehrung": "EUR"},
                     "kostenpositionen": [
                         {
-                            "positionstitel": "Mudders Preisstaffel",
+                            "positionstitel": "Mudders kostenposition",
                             "einzelpreis": {
-                                "status": "ENDGUELTIG",
                                 "einheit": "EUR",
-                                "wert": "3.5",
+                                "status": "ENDGUELTIG",
                                 "bezugswert": "KWH",
+                                "wert": "3.5",
                             },
-                            "bis": None,
-                            "von": None,
                             "menge": None,
                             "zeitmenge": None,
-                            "artikelbezeichnung": "Dei Mudder ihr Preisstaffel",
+                            "von": None,
+                            "artikelbezeichnung": "Dei Mudder ihr kostenposition",
                             "artikeldetail": None,
+                            "bis": None,
                             "betragKostenposition": {"waehrung": "EUR", "wert": "12.5"},
                         }
                     ],
+                    "summeKostenblock": {"waehrung": "EUR", "wert": "12.5"},
                 },
                 id="minimal",
             ),
