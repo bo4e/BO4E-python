5e9dbdb0b468015e784e0c33bb17800d0c084588
konstantin <konstantin.klein@hochfrequenz.de>
2022-01-05 18:38:08 +0100

    remove unused import

--- src/bo4e/bo/standorteigenschaften.py
+++ src/bo4e/bo/standorteigenschaften.py
@@ -6,7 +6,6 @@ from typing import List, Optional
 
 import attr
 from marshmallow import fields, post_load
-from marshmallow_enum import EnumField  # type:ignore[import]
 
 from bo4e.bo.geschaeftsobjekt import Geschaeftsobjekt, GeschaeftsobjektSchema
 from bo4e.com.standorteigenschaftenallgemein import StandorteigenschaftenAllgemein, StandorteigenschaftenAllgemeinSchema
