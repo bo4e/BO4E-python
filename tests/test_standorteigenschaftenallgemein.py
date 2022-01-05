a721ec0a43b7020e09f246357b91d3617d6fa41e
konstantin <konstantin.klein@hochfrequenz.de>
2022-01-05 18:33:05 +0100

    Implement BO Standorteigenschaften

--- tests/test_standorteigenschaftenallgemein.py
+++ tests/test_standorteigenschaftenallgemein.py
@@ -3,32 +3,30 @@ import pytest  # type:ignore[import]
 from bo4e.com.standorteigenschaftenallgemein import StandorteigenschaftenAllgemein, StandorteigenschaftenAllgemeinSchema
 from tests.serialization_helper import assert_serialization_roundtrip  # type:ignore[import]
 
+example_standorteigenschaften_allgemein = StandorteigenschaftenAllgemein(
+    postleitzahl="66646",
+    ort="Alsweiler",
+    kreisname="St. Wendel",
+    gemeindename="Marpingen",
+    gemeindekennziffer="10 0 46 112",
+    gemeindeeinwohner=9961,
+    bundesland="Saarland",
+)
+example_standorteigenschaften_allgemein_dict = {
+    "postleitzahl": "66646",
+    "ort": "Alsweiler",
+    "kreisname": "St. Wendel",
+    "gemeindename": "Marpingen",
+    "gemeindekennziffer": "10 0 46 112",
+    "gemeindeeinwohner": 9961,
+    "bundesland": "Saarland",
+}
+
 
 class TestStandorteigenschaftenAllgemein:
     @pytest.mark.parametrize(
         "standorteigenschaftenallgemein, expected_json_dict",
-        [
-            pytest.param(
-                StandorteigenschaftenAllgemein(
-                    postleitzahl="66646",
-                    ort="Alsweiler",
-                    kreisname="St. Wendel",
-                    gemeindename="Marpingen",
-                    gemeindekennziffer="10 0 46 112",
-                    gemeindeeinwohner=9961,
-                    bundesland="Saarland",
-                ),
-                {
-                    "postleitzahl": "66646",
-                    "ort": "Alsweiler",
-                    "kreisname": "St. Wendel",
-                    "gemeindename": "Marpingen",
-                    "gemeindekennziffer": "10 0 46 112",
-                    "gemeindeeinwohner": 9961,
-                    "bundesland": "Saarland",
-                },
-            )
-        ],
+        [pytest.param(example_standorteigenschaften_allgemein, example_standorteigenschaften_allgemein_dict)],
     )
     def test_standorteigenschaftenallgemein_serialization_roundtrip(
         self, standorteigenschaftenallgemein: StandorteigenschaftenAllgemein, expected_json_dict: dict
