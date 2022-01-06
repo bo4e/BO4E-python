c4027f811275eca7b2978d3e74ec77290a1ec9ce
konstantin <konstantin.klein@hochfrequenz.de>
2022-01-06 18:18:36 +0100

    📝Add Docstrings to older Business Objects (#308)
    * add missing docs to ansprechpartner
    
    * add missing docs to Zaehler
    
    * add exklusiv keyword
    
    * add docs to geschaeftsobjekt
    
    * add docs to BO Vertrag
    
    * add docs to messlokation
    
    * add docs for marktteilnehmer
    
    * add docs to marktlokation
    
    * add docs to geschaeftspartner
    
    * fix long line

--- src/bo4e/bo/geschaeftspartner.py
+++ src/bo4e/bo/geschaeftspartner.py
@@ -20,27 +20,64 @@ from bo4e.enum.kontaktart import Kontaktart
 @attr.s(auto_attribs=True, kw_only=True)
 class Geschaeftspartner(Geschaeftsobjekt):
     """
-    Object containing information about a Geschaeftspartner
+    Mit diesem Objekt können Geschäftspartner übertragen werden.
+    Sowohl Unternehmen, als auch Privatpersonen können Geschäftspartner sein.
+    Hinweis: Marktteilnehmer haben ein eigenes BO, welches sich von diesem BO ableitet.
+    Hier sollte daher keine Zuordnung zu Marktrollen erfolgen.
     """
 
     # required attributes
     bo_typ: BoTyp = attr.ib(default=BoTyp.GESCHAEFTSPARTNER)
     name1: str
+    """
+    Erster Teil des Namens.
+    Hier kann der Firmenname oder bei Privatpersonen beispielsweise der Nachname dagestellt werden.
+    Beispiele: Yellow Strom GmbH oder Hagen
+    """
+    # todo: replace name1/2/3 with something more readable. no one wants to deal with that. maybe serialize as name1/2/3
+    # but resolve to readable python fields under the hood
+
     gewerbekennzeichnung: bool
+    """
+    Kennzeichnung ob es sich um einen Gewerbe/Unternehmen (gewerbeKennzeichnung = true)
+    oder eine Privatperson handelt. (gewerbeKennzeichnung = false)
+    """
+    #: Rollen, die die Geschäftspartner inne haben (z.B. Interessent, Kunde)
     geschaeftspartnerrolle: List[Geschaeftspartnerrolle] = attr.ib(validator=attr.validators.instance_of(List))
+    # todo: rename to plural
 
     # optional attributes
+    #: Die Anrede für den GePa, Z.B. "Herr"
     anrede: Anrede = attr.ib(default=None)
     name2: Optional[str] = attr.ib(default=None)
+    """
+    Zweiter Teil des Namens.
+    Hier kann der eine Erweiterung zum Firmennamen oder bei Privatpersonen beispielsweise der Vorname dagestellt werden.
+    Beispiele: Bereich Süd oder Nina
+    """
+
     name3: Optional[str] = attr.ib(default=None)
+    """
+    Dritter Teil des Namens.
+    Hier können weitere Ergänzungen zum Firmennamen oder bei Privatpersonen Zusätze zum Namen dagestellt werden.
+    Beispiele: und Afrika oder Sängerin
+    """
+    #: Handelsregisternummer des Geschäftspartners
     hrnummer: Optional[str] = attr.ib(default=None)
+    #: Amtsgericht bzw Handelsregistergericht, das die Handelsregisternummer herausgegeben hat
     amtsgericht: Optional[str] = attr.ib(default=None)
+    #: Bevorzugte Kontaktwege des Geschäftspartners
     kontaktweg: List[Kontaktart] = attr.ib(default=[])
+    #: Die Steuer-ID des Geschäftspartners; Beispiel: "DE 813281825"
     umsatzsteuer_id: Optional[str] = attr.ib(default=None)
+    #: Die Gläubiger-ID welche im Zahlungsverkehr verwendet wird; Z.B. "DE 47116789"
     glaeubiger_id: Optional[str] = attr.ib(default=None)
+    #: E-Mail-Adresse des Ansprechpartners. Z.B. "info@hochfrequenz.de"
     e_mail_adresse: Optional[str] = attr.ib(default=None)
+    #: Internetseite des Marktpartners
     website: Optional[str] = attr.ib(default=None)
-    partneradresse: Adresse = attr.ib(default=None)
+    #: Adressen der Geschäftspartner, an denen sich der Hauptsitz befindet
+    partneradresse: Adresse = attr.ib(default=None)  # todo: is it plural or not? the docs are bad
 
 
 class GeschaeftspartnerSchema(GeschaeftsobjektSchema):
