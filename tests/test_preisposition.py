b3568c7df2c02b823af9635c0e6cbe912ffde3ee
konstantin <konstantin.klein@hochfrequenz.de>
2022-01-10 09:57:07 +0100

    Implement BO Preisblatt

--- tests/test_preisposition.py
+++ tests/test_preisposition.py
@@ -14,27 +14,27 @@ from bo4e.enum.zeiteinheit import Zeiteinheit
 from tests.serialization_helper import assert_serialization_roundtrip  # type:ignore[import]
 from tests.test_preisstaffel import example_preisstaffel  # type:ignore[import]
 
+example_preisposition = Preisposition(
+    berechnungsmethode=Kalkulationsmethode.AP_TRANSPORT_ODER_VERTEILNETZ,
+    leistungstyp=Leistungstyp.SONSTIGER_PREIS,
+    leistungsbezeichnung="Foo",
+    preiseinheit=Waehrungseinheit.EUR,
+    bezugsgroesse=Mengeneinheit.KVARH,
+    zeitbasis=Zeiteinheit.HALBJAHR,
+    tarifzeit=Tarifzeit.TZ_HT,
+    bdew_artikelnummer=BDEWArtikelnummer.AUSGLEICHSENERGIE_UNTERDECKUNG,
+    zonungsgroesse=Bemessungsgroesse.BENUTZUNGSDAUER,
+    freimenge_blindarbeit=Decimal(50),  # %
+    freimenge_leistungsfaktor=Decimal(1.0),
+    preisstaffeln=[example_preisstaffel],
+)
+
 
 class TestPreisposition:
     @pytest.mark.parametrize(
         "preisposition",
         [
-            pytest.param(
-                Preisposition(
-                    berechnungsmethode=Kalkulationsmethode.AP_TRANSPORT_ODER_VERTEILNETZ,
-                    leistungstyp=Leistungstyp.SONSTIGER_PREIS,
-                    leistungsbezeichnung="Foo",
-                    preiseinheit=Waehrungseinheit.EUR,
-                    bezugsgroesse=Mengeneinheit.KVARH,
-                    zeitbasis=Zeiteinheit.HALBJAHR,
-                    tarifzeit=Tarifzeit.TZ_HT,
-                    bdew_artikelnummer=BDEWArtikelnummer.AUSGLEICHSENERGIE_UNTERDECKUNG,
-                    zonungsgroesse=Bemessungsgroesse.BENUTZUNGSDAUER,
-                    freimenge_blindarbeit=Decimal(50),  # %
-                    freimenge_leistungsfaktor=Decimal(1.0),
-                    preisstaffeln=[example_preisstaffel],
-                )
-            ),
+            pytest.param(example_preisposition),
         ],
     )
     def test_serialization_roundtrip(self, preisposition: Preisposition):
