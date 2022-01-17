18b2bd894981905f1dd3e48746beb24ea2b3516e
Franziska Vesely <franziska.vesely@hochfrequenz.de>
2022-01-17 15:52:08 +0100

    fix copy paste error

--- tests/test_fremdkostenblock.py
+++ tests/test_fremdkostenblock.py
@@ -82,7 +82,7 @@ class TestFremdkostenblock:
     )
     def test_serialization_roundtrip(self, fremdkostenblock, expected_json_dict):
         """
-        Test de-/serialisation of Angebotsteil with minimal attributes.
+        Test de-/serialisation of Fremdkostenblock with minimal attributes.
         """
         assert_serialization_roundtrip(fremdkostenblock, FremdkostenblockSchema(), expected_json_dict)
 
