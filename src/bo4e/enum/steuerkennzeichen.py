0957dce14333e6062c1b8f71bfccff96c98939c7
konstantin <konstantin.klein@hochfrequenz.de>
2022-06-15 11:33:43 +0200

    🐛Add `UST_16` to Enum Steuerkennzeichen (#369)
    fixes #356

--- src/bo4e/enum/steuerkennzeichen.py
+++ src/bo4e/enum/steuerkennzeichen.py
@@ -9,6 +9,7 @@ class Steuerkennzeichen(StrEnum):
 
     UST_0 = "UST_0"  #: Keine Umsatzsteuer, bzw. nicht steuerbar.
     UST_19 = "UST_19"  #: Umsatzsteuer 19%
+    UST_16 = "UST_16"  #: Umsatzsteuer 16%
     UST_7 = "UST_7"  #: Umsatzsteuer 7%
     VST_0 = "VST_0"  #: Keine Vorsteuer, bzw. nicht steuerbar.
     VST_19 = "VST_19"  #: Vorsteuer 19%
