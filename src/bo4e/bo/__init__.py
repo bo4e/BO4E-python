6b409902465a109aae9c6418bc3ec042c3d067a9
kevin <kevin.krechan@hochfrequenz.de>
2022-12-02 16:11:40 +0100

    🎨 Add imports in bo __init__.py

---
+++ src/bo4e/bo/__init__.py
@@ -0,0 +1,36 @@
+"""
+This module makes it easier to import a business object.
+For example: `from bo4e.bo import Marktlokation`
+"""
+
+from .angebot import Angebot
+from .ansprechpartner import Ansprechpartner
+from .ausschreibung import Ausschreibung
+from .buendelvertrag import Buendelvertrag
+from .energiemenge import Energiemenge
+from .fremdkosten import Fremdkosten
+from .geschaeftsobjekt import Geschaeftsobjekt
+from .geschaeftspartner import Geschaeftspartner
+from .kosten import Kosten
+from .lastgang import Lastgang
+from .marktlokation import Marktlokation
+from .marktteilnehmer import Marktteilnehmer
+from .messlokation import Messlokation
+from .netznutzungsrechnung import Netznutzungsrechnung
+from .preisblatt import Preisblatt
+from .preisblattdienstleistung import PreisblattDienstleistung
+from .preisblatthardware import PreisblattHardware
+from .preisblattkonzessionsabgabe import PreisblattKonzessionsabgabe
+from .preisblattmessung import PreisblattMessung
+from .preisblattnetznutzung import PreisblattNetznutzung
+from .rechnung import Rechnung
+from .region import Region
+from .regionaltarif import Regionaltarif
+from .standorteigenschaften import Standorteigenschaften
+from .tarif import Tarif
+from .tarifinfo import Tarifinfo
+from .tarifkosten import Tarifkosten
+from .tarifpreisblatt import Tarifpreisblatt
+from .vertrag import Vertrag
+from .zaehler import Zaehler
+from .zeitreihe import Zeitreihe
