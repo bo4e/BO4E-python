1c296efcbea24a5119f737d52f8c98d4f582f063
konstantin <konstantin.klein@hochfrequenz.de>
2023-09-28 09:59:00 +0200

    separate generation and validation of json schemas

--- json_schemas/generate_json_schemas.py
+++ json_schemas/generate_json_schemas.py
@@ -1,6 +1,5 @@
 """
 This script is run in the tox 'json_schemas' environment.
-It creates json schema files as described in the README.md in the same directory.
 """
 
 import importlib
@@ -8,24 +7,54 @@ import inspect
 import json
 import pathlib
 import pkgutil
+from typing import Literal
 
-pkgs = ["bo", "com"]
-for pkg in pkgs:
-    modls = [
-        name for _, name, _ in pkgutil.iter_modules([str(pathlib.Path(__file__).parent.parent / "src" / "bo4e" / pkg)])
-    ]
-    for x in modls:
-        modl_name = f"bo4e.{pkg}.{x}"
-        modl = importlib.import_module(modl_name)
-        # pylint: disable=cell-var-from-loop
-        cls_list = inspect.getmembers(modl, lambda member: inspect.isclass(member) and member.__module__ == modl_name)
-        for name, cls in cls_list:
-            this_directory = pathlib.Path(__file__).parent.absolute()
-            file_path = this_directory / pkg / (name + ".json")  # pylint:disable=invalid-name
-            schema_json_dict = cls.model_json_schema()
-            if "definitions" in schema_json_dict:
-                for definition in schema_json_dict["definitions"].values():
-                    # this sanitizing step is necessary since python 3.11
-                    definition["description"] = definition["description"].strip()
-            with open(file_path, "w+", encoding="utf-8") as json_schema_file:
-                json_schema_file.write(json.dumps(schema_json_dict, indent=4))
+import click
+
+
+@click.command()
+@click.option(
+    "--mode",
+    "-m",
+    help="use 'validate' to validate existing schemas or 'generate' to generate new schemas",
+    required=True,
+    type=click.Choice(["validate", "generate"]),
+)
+def generate_json_schemas(mode: Literal["validate", "generate"]):
+    """generate json schemas for all BOs and COMs"""
+    for pkg in ["bo", "com"]:
+        modls = [
+            name
+            for _, name, _ in pkgutil.iter_modules([str(pathlib.Path(__file__).parent.parent / "src" / "bo4e" / pkg)])
+        ]
+        for x in modls:
+            modl_name = f"bo4e.{pkg}.{x}"
+            modl = importlib.import_module(modl_name)
+            # pylint: disable=cell-var-from-loop
+            cls_list = inspect.getmembers(
+                modl, lambda member: inspect.isclass(member) and member.__module__ == modl_name
+            )
+            for name, cls in cls_list:
+                this_directory = pathlib.Path(__file__).parent.absolute()
+                file_path = this_directory / pkg / (name + ".json")  # pylint:disable=invalid-name
+                schema_json_dict = cls.model_json_schema()
+                if "definitions" in schema_json_dict:
+                    for definition in schema_json_dict["definitions"].values():
+                        # this sanitizing step is necessary since python 3.11
+                        definition["description"] = definition["description"].strip()
+                if mode == "validate":
+                    with open(file_path, "r", encoding="utf-8") as json_schema_file:
+                        existing_schema = json.load(json_schema_file)
+                    if schema_json_dict != existing_schema:
+                        raise ValueError(f"Schema for {name} has changed. Please run this script with mode 'generate'.")
+                elif mode == "generate":
+                    with open(file_path, "w+", encoding="utf-8") as json_schema_file:
+                        json_schema_file.write(
+                            json.dumps(schema_json_dict, indent=4, sort_keys=True, ensure_ascii=False)
+                        )
+                else:
+                    raise ValueError(f"Unknown mode '{mode}'")
+
+
+def __main__():
+    pass
