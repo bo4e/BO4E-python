410cbb330e743c5e2131995e637abca81d18ea4e
DeltaDaniel <daniel.dernbach@hochfrequenz.de>
2023-10-06 15:37:03 +0200

    🗑️ removed COM zeitreihenwertkompakt and corresponding json

--- src/bo4e/com/zeitreihenwert.py
+++ src/bo4e/com/zeitreihenwert.py
@@ -3,14 +3,17 @@ Contains Zeitreihenwert class
 and corresponding marshmallow schema for de-/serialization
 """
 from datetime import datetime
+from decimal import Decimal
 from typing import Optional
 
-from bo4e.com.zeitreihenwertkompakt import Zeitreihenwertkompakt
+from bo4e.com.com import COM
+from bo4e.enum.messwertstatus import Messwertstatus
+from bo4e.enum.messwertstatuszusatz import Messwertstatuszusatz
 
 # pylint: disable=too-few-public-methods
 
 
-class Zeitreihenwert(Zeitreihenwertkompakt):
+class Zeitreihenwert(COM):
     """
     Abbildung eines Zeitreihenwertes bestehend aus Zeitraum, Wert und Statusinformationen.
 
@@ -23,10 +26,17 @@ class Zeitreihenwert(Zeitreihenwertkompakt):
 
     """
 
-    # required attributes
     datum_uhrzeit_von: Optional[
         datetime
     ] = None  #: Datum Uhrzeit mit Auflösung Sekunden an dem das Messintervall begonnen wurde (inklusiv)
     datum_uhrzeit_bis: Optional[
         datetime
     ] = None  #: Datum Uhrzeit mit Auflösung Sekunden an dem das Messintervall endet (exklusiv)
+
+    wert: Optional[Decimal] = None  #: Der im Zeitintervall gültige Wert.
+
+    #: Der Status gibt an, wie der Wert zu interpretieren ist, z.B. in Berechnungen.
+    status: Optional[Messwertstatus] = None
+
+    #: Eine Zusatzinformation zum Status, beispielsweise ein Grund für einen fehlenden Wert.
+    statuszusatz: Optional[Messwertstatuszusatz] = None
