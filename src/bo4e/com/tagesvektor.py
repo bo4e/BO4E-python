de0e344bd451594624b3ea97e4b760392a7c9f8c
DeltaDaniel <daniel.dernbach@hochfrequenz.de>
2023-10-06 15:31:22 +0200

    Revert "🗑️removed COM tagesvektor and COM zeitreihenwertkompakt"
    This reverts commit b72aba42106f3ab365847b6f5854dd5c1e38aab2.

---
+++ src/bo4e/com/tagesvektor.py
@@ -0,0 +1,40 @@
+"""
+Contains Tagesvektor class and corresponding marshmallow schema for de-/serialization
+"""
+from datetime import datetime
+from typing import Optional
+
+from bo4e.com.com import COM
+from bo4e.com.zeitreihenwertkompakt import Zeitreihenwertkompakt
+
+# pylint: disable=too-few-public-methods
+# pylint: disable=no-name-in-module
+
+
+class Tagesvektor(COM):
+    """
+    Abbildung eines Tagesvektors eines beliebigen äquidistanten Zeitrasters
+
+    .. raw:: html
+
+        <object data="../_static/images/bo4e/com/Tagesvektor.svg" type="image/svg+xml"></object>
+
+    .. HINT::
+        `Tagesvektor JSON Schema <https://json-schema.app/view/%23?url=https://raw.githubusercontent.com/Hochfrequenz/BO4E-python/main/json_schemas/com/Tagesvektor.json>`_
+
+    """
+
+    # required attributes
+    # for the validator see https://github.com/Hochfrequenz/BO4E-python/issues/261
+    tag: Optional[datetime] = None
+    """
+    Der Zeitpunkt ab dem die Werte ermittelt wurden.
+    Es kann entweder der Beginn des Strom- oder Gastages verwendet werden.
+    Der Zeitpunkt sollte eindeutig sein, d.h. sowohl Datum+Uhrzeit als auch den UTC-Offset spezifizieren.
+    """
+    # for the validator see also https://github.com/Hochfrequenz/BO4E-python/issues/262
+    werte: Optional[list[Zeitreihenwertkompakt]] = None
+    """
+    Die Werte am angegebenen Tag;
+    In Kombination aus Zeitintervall und Tag lassen sich die Zeiten der Werte eindeutig konstruieren.
+    """
---
+++ src/bo4e/com/zeitreihenwertkompakt.py
@@ -0,0 +1,37 @@
+"""
+Contains Zeitreihenwertkompakt class
+and corresponding marshmallow schema for de-/serialization
+"""
+from decimal import Decimal
+from typing import Optional
+
+from bo4e.com.com import COM
+from bo4e.enum.messwertstatus import Messwertstatus
+from bo4e.enum.messwertstatuszusatz import Messwertstatuszusatz
+
+# pylint: disable=too-few-public-methods
+
+
+class Zeitreihenwertkompakt(COM):
+    """
+    Abbildung eines kompakten Zeitreihenwertes in dem ausschliesslich der Wert und Statusinformationen stehen.
+
+    .. raw:: html
+
+        <object data="../_static/images/bo4e/com/Zeitreihenwertkompakt.svg" type="image/svg+xml"></object>
+
+    .. HINT::
+        `Zeitreihenwertkompakt JSON Schema <https://json-schema.app/view/%23?url=https://raw.githubusercontent.com/Hochfrequenz/BO4E-python/main/json_schemas/com/Zeitreihenwertkompakt.json>`_
+
+    """
+
+    # required attributes
+    wert: Optional[Decimal] = None  #: Der im Zeitintervall gültige Wert.
+
+    # optional attributes
+
+    #: Der Status gibt an, wie der Wert zu interpretieren ist, z.B. in Berechnungen.
+    status: Optional[Messwertstatus] = None
+
+    #: Eine Zusatzinformation zum Status, beispielsweise ein Grund für einen fehlenden Wert.
+    statuszusatz: Optional[Messwertstatuszusatz] = None
