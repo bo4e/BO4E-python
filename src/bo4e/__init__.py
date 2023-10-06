2ddf771f5d31ce0835e159ad9b0fe65f6a336732
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2023-10-06 16:36:33 +0200

    🩹

--- src/bo4e/__init__.py
+++ src/bo4e/__init__.py
@@ -1,1 +1,10 @@
+"""
+BO4E-Python - Python implementation of the BO4E standard
+
+BO4E-Python is a Python implementation of the BO4E standard. BO4E is a standard for the exchange of
+business objects in the energy industry. BO4E-Python is an open-source software released under the Apache-2.0
+license.
+
+The BO4E version can be queried using `bo4e.__version__`.
+"""
 from .version import __version__
