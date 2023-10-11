0ec483bb3a4a3023d95bce0c21b6a3816e11ca8e
kevin <kevin.krechan@hochfrequenz.de>
2023-10-11 19:14:00 +0200

    ✅ Test ansprechpartner

--- tests/test_ansprechpartner.py
+++ tests/test_ansprechpartner.py
@@ -12,108 +12,26 @@ from bo4e import (
     Typ,
     Zustaendigkeit,
 )
+from tests.serialization_helper import assert_serialization_roundtrip
 
 
 class TestAnsprechpartner:
-    def test_de_serialisation_minimal_attributes(self) -> None:
+    def test_serialization_roundtrip(self) -> None:
         """
-        Test de-/serialisation of Ansprechpartner only with required attributes
-        """
-        ansprechpartner = Ansprechpartner(
-            nachname="Müller-Schmidt",
-            geschaeftspartner=Geschaeftspartner(
-                anrede=Anrede.FRAU,
-                name1="von Sinnen",
-                name2="Helga",
-                name3=None,
-                ist_gewerbe=True,
-                hrnummer="HRB 254466",
-                amtsgericht="Amtsgericht München",
-                kontaktweg=[Kontaktart.E_MAIL],
-                umsatzsteuer_id="DE267311963",
-                glaeubiger_id="DE98ZZZ09999999999",
-                e_mail_adresse="test@bo4e.de",
-                website="bo4e.de",
-                geschaeftspartnerrolle=[Geschaeftspartnerrolle.DIENSTLEISTER],
-                partneradresse=Adresse(
-                    postleitzahl="12345",
-                    ort="Sankt Musterweiler",
-                    strasse="Mühlenweg",
-                    hausnummer="540a",
-                ),
-            ),
-        )
-        assert ansprechpartner.version is not None, "versionstruktur was not automatically set"
-        assert ansprechpartner.typ is Typ.ANSPRECHPARTNER, "_typ was not automatically set"
-
-        json_string = ansprechpartner.model_dump_json(by_alias=True)
-        assert "Müller-Schmidt" in json_string
-        assert "Mühlenweg" in json_string
-        assert '"FRAU"' in json_string
-
-        deserialized_ansprechpartner = Ansprechpartner.model_validate_json(json_string)
-
-        assert isinstance(deserialized_ansprechpartner, Ansprechpartner)
-        assert isinstance(deserialized_ansprechpartner.geschaeftspartner, Geschaeftspartner)
-        assert deserialized_ansprechpartner == ansprechpartner
-
-    def test_de_serialisation_maximal_attributes(self) -> None:
-        """
-        Test de-/serialisation of Ansprechpartner only with required attributes
+        Test Test de-/serialisation roundtrip of Ansprechpartner
         """
         ansprechpartner = Ansprechpartner(
             nachname="Müller-Schmidt",
-            geschaeftspartner=Geschaeftspartner(
-                anrede=Anrede.FRAU,
-                name1="von Sinnen",
-                name2="Helga",
-                name3=None,
-                ist_gewerbe=True,
-                hrnummer="HRB 254466",
-                amtsgericht="Amtsgericht München",
-                kontaktweg=[Kontaktart.E_MAIL],
-                umsatzsteuer_id="DE267311963",
-                glaeubiger_id="DE98ZZZ09999999999",
-                e_mail_adresse="test@bo4e.de",
-                website="bo4e.de",
-                geschaeftspartnerrolle=[Geschaeftspartnerrolle.DIENSTLEISTER],
-                partneradresse=Adresse(
-                    postleitzahl="12345",
-                    ort="Sankt Musterweiler",
-                    strasse="Mühlenweg",
-                    hausnummer="540a",
-                ),
-            ),
+            geschaeftspartner=Geschaeftspartner(),
             anrede=Anrede.EHELEUTE,
             individuelle_anrede="Künstler",
             titel=Titel.PROF_DR,
             vorname="Hans",
             e_mail_adresse="hans.müller@getrei.de",
             kommentar="does this thing work?",
-            adresse=Adresse(
-                postleitzahl="12345",
-                ort="Sankt Musterweiler",
-                strasse="Mühlenweg",
-                hausnummer="540a",
-            ),
-            rufnummer=Rufnummer(nummerntyp=Rufnummernart.RUF_DURCHWAHL, rufnummer="112358132134"),
-            zustaendigkeit=Zustaendigkeit(
-                themengebiet=Themengebiet.MARKTKOMMUNIKATION, jobtitel="Schatzmeister", abteilung="unten rechts"
-            ),
+            adresse=Adresse(),
+            rufnummer=Rufnummer(),
+            zustaendigkeit=Zustaendigkeit(),
         )
-        assert ansprechpartner.version is not None, "versionstruktur was not automatically set"
-        assert ansprechpartner.typ is Typ.ANSPRECHPARTNER, "_typ was not automatically set"
 
-        json_string = ansprechpartner.model_dump_json(by_alias=True)
-        assert "Müller-Schmidt" in json_string
-        assert "Mühlenweg" in json_string
-        assert "PROF_DR" in json_string
-
-        deserialized_ansprechpartner = Ansprechpartner.model_validate_json(json_string)
-
-        assert isinstance(deserialized_ansprechpartner, Ansprechpartner)
-        assert isinstance(deserialized_ansprechpartner.geschaeftspartner, Geschaeftspartner)
-        assert isinstance(deserialized_ansprechpartner.adresse, Adresse)
-        assert isinstance(deserialized_ansprechpartner.rufnummer, Rufnummer)
-        assert isinstance(deserialized_ansprechpartner.zustaendigkeit, Zustaendigkeit)
-        assert deserialized_ansprechpartner == ansprechpartner
+        assert_serialization_roundtrip(ansprechpartner)
