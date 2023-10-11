fc363db15c340694bdd17eda3f55f100f509fdc2
kevin <kevin.krechan@hochfrequenz.de>
2023-10-11 19:33:10 +0200

    ✅ Test fremdkostenposition

--- tests/test_fremdkostenposition.py
+++ tests/test_fremdkostenposition.py
@@ -5,7 +5,7 @@ from typing import Any, Dict
 import pytest
 from pydantic import ValidationError
 
-from bo4e import Betrag, Fremdkostenposition, Mengeneinheit, Preis, Preisstatus, Waehrungscode, Waehrungseinheit
+from bo4e import Betrag, Fremdkostenposition, Menge, Mengeneinheit, Preis, Preisstatus, Waehrungscode, Waehrungseinheit
 from tests.serialization_helper import assert_serialization_roundtrip
 from tests.test_menge import example_menge
 from tests.test_sigmoidparameter import example_sigmoidparameter
@@ -22,24 +22,16 @@ class TestFremdkostenposition:
                     bis=datetime(2014, 5, 1, tzinfo=timezone.utc),
                     artikelbezeichnung="Deim Vadder sei Preisstaffel",
                     link_preisblatt="http://foo.bar/",
-                    zeitmenge=example_menge,
-                    einzelpreis=Preis(
-                        wert=Decimal(3.50),
-                        einheit=Waehrungseinheit.EUR,
-                        bezugswert=Mengeneinheit.KWH,
-                        status=Preisstatus.ENDGUELTIG,
-                    ),
-                    betrag_kostenposition=Betrag(
-                        waehrung=Waehrungscode.EUR,
-                        wert=Decimal(12.5),
-                    ),
-                    menge=example_menge,
+                    zeitmenge=Menge(),
+                    einzelpreis=Preis(),
+                    betrag_kostenposition=Betrag(),
+                    menge=Menge(),
                     artikeldetail="foo",
                     marktpartnercode="986543210123",
                     marktpartnername="Mein MP",
                     gebietcode_eic="not an eic code but validation will follow in ticket 146",
                 ),
-                id="required and optional attributes",
+                id="all attributes at first level",
             ),
         ],
     )
