e1100a43b75b430d02fbb07c70bf79ec205b26e0
DeltaDaniel <daniel.dernbach@hochfrequenz.de>
2023-10-23 12:13:10 +0200

    black

--- src/bo4e/bo/lastgang.py
+++ src/bo4e/bo/lastgang.py
@@ -22,15 +22,15 @@ from bo4e.enum.sparte import Sparte
 
 class Lastgang(Geschaeftsobjekt):
     """
-    Modell zur Abbildung eines Lastganges;
-In diesem Modell werden die Messwerte mit einem vollständigen Zeitintervall angegeben und es bietet daher eine hohe Flexibilität in der Übertragung jeglicher zeitlich veränderlicher Messgrössen.
+        Modell zur Abbildung eines Lastganges;
+    In diesem Modell werden die Messwerte mit einem vollständigen Zeitintervall angegeben und es bietet daher eine hohe Flexibilität in der Übertragung jeglicher zeitlich veränderlicher Messgrössen.
 
-    .. raw:: html
+        .. raw:: html
 
-        <object data="../_static/images/bo4e/bo/Lastgang.svg" type="image/svg+xml"></object>
+            <object data="../_static/images/bo4e/bo/Lastgang.svg" type="image/svg+xml"></object>
 
-    .. HINT::
-        `Lastgang JSON Schema <https://json-schema.app/view/%23?url=https://raw.githubusercontent.com/Hochfrequenz/BO4E-python/main/json_schemas/bo/Lastgang.json>`_
+        .. HINT::
+            `Lastgang JSON Schema <https://json-schema.app/view/%23?url=https://raw.githubusercontent.com/Hochfrequenz/BO4E-python/main/json_schemas/bo/Lastgang.json>`_
 
     """
 
