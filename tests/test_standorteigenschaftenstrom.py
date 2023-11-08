27be46b78a5ced3bd3ade7e2815c96e49a60b006
Stefan Heese <stefan.heese@hochfrequenz.de>
2023-11-08 15:22:15 +0100

    ✅Test StandorteigenschaftenStrom

--- tests/test_standorteigenschaftenstrom.py
+++ tests/test_standorteigenschaftenstrom.py
@@ -6,21 +6,22 @@ from pydantic import ValidationError
 from bo4e import StandorteigenschaftenStrom
 from tests.serialization_helper import assert_serialization_roundtrip
 
-example_standorteigenschaften_strom = StandorteigenschaftenStrom(
-    regelzone="Transnet BW",
-    bilanzierungsgebiet_eic="11YW-ALBSTADT--5",
-    regelzone_eic="10YDE-ENBW-----N",
-)
-
 
 class TestStandorteigenschaftenStrom:
     @pytest.mark.parametrize(
         "standorteigenschaften_strom",
         [
             pytest.param(
-                example_standorteigenschaften_strom,
+                StandorteigenschaftenStrom(
+                    regelzone="Transnet BW",
+                    bilanzierungsgebiet_eic="11YW-ALBSTADT--5",
+                    regelzone_eic="10YDE-ENBW-----N",
+                ),
             )
         ],
     )
     def test_serialization_roundtrip(self, standorteigenschaften_strom: StandorteigenschaftenStrom) -> None:
+        """
+        Test de-/serialisation.
+        """
         assert_serialization_roundtrip(standorteigenschaften_strom)
