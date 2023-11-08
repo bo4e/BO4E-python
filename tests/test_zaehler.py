5eeda492b220b3732411cebb83d8b804278d5a43
Stefan Heese <stefan.heese@hochfrequenz.de>
2023-11-08 12:55:15 +0100

    ✅Test Zähler

--- tests/test_zaehler.py
+++ tests/test_zaehler.py
@@ -20,31 +20,27 @@ from tests.serialization_helper import assert_serialization_roundtrip
 
 
 class TestZaehler:
-    def test_de_serialisation(self) -> None:
+    @pytest.mark.parametrize(
+        "zaehler",
+        [
+            pytest.param(
+                Zaehler(
+                    zaehlernummer="000111222",
+                    sparte=Sparte.STROM,
+                    zaehlerauspraegung=Zaehlerauspraegung.EINRICHTUNGSZAEHLER,
+                    zaehlwerke=[Zaehlwerk()],
+                    zaehlertyp=Zaehlertyp.DREHSTROMZAEHLER,
+                    tarifart=Tarifart.ZWEITARIF,
+                    zaehlerkonstante=Decimal(0.9),
+                    eichung_bis=datetime(2022, 1, 1, 0, 0, 0),
+                    zusatz_attribute=[ZusatzAttribut(name="zaehler im anderen system", wert="7890")],
+                    letzte_eichung=datetime(2019, 6, 30, 0, 0, 0),
+                ),
+            ),
+        ],
+    )
+    def test_serialization_roundtrip(self, zaehler: Zaehler) -> None:
         """
-        Test de-/serialisation of Zaehler only with required attributes
+        Test de-/serialisation of Zaehler.
         """
-
-        zaehler = Zaehler(
-            zaehlernummer="000111222",
-            sparte=Sparte.STROM,
-            zaehlerauspraegung=Zaehlerauspraegung.EINRICHTUNGSZAEHLER,
-            zaehlwerke=[
-                Zaehlwerk(
-                    zaehlwerk_id="98765",
-                    einheit=Mengeneinheit.KW,
-                    richtung=Energierichtung.EINSP,
-                    bezeichnung="my zaehlwerk",
-                    obis_kennzahl="1-0:1.8.1",
-                    wandlerfaktor=Decimal(0.95),
-                )
-            ],
-            zaehlertyp=Zaehlertyp.DREHSTROMZAEHLER,
-            tarifart=Tarifart.ZWEITARIF,
-            zaehlerkonstante=Decimal(0.9),
-            eichung_bis=datetime(2022, 1, 1, 0, 0, 0),
-            zusatz_attribute=[ZusatzAttribut(name="zaehler im anderen system", wert="7890")],
-            letzte_eichung=datetime(2019, 6, 30, 0, 0, 0),
-        )
-
         assert_serialization_roundtrip(zaehler)
