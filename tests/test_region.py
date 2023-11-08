d6c8db368732dda0571e891fc1915746d4428f48
Stefan Heese <stefan.heese@hochfrequenz.de>
2023-11-08 14:27:49 +0100

    ✅Test Region

--- tests/test_region.py
+++ tests/test_region.py
@@ -1,9 +1,8 @@
 import pytest
 from pydantic import ValidationError
 
-from bo4e import Region
+from bo4e import Region, Regionskriterium
 from tests.serialization_helper import assert_serialization_roundtrip
-from tests.test_regionskriterium import example_regionskriterium
 
 
 class TestRegion:
@@ -13,26 +12,16 @@ class TestRegion:
             pytest.param(
                 Region(
                     bezeichnung="Bikini Bottom",
-                    positiv_liste=[example_regionskriterium],
-                    negativ_liste=[example_regionskriterium],
+                    positiv_liste=[Regionskriterium()],
+                    negativ_liste=[Regionskriterium()],
                 ),
                 id="max attributes",
             ),
-            pytest.param(
-                Region(bezeichnung="Bikini Bottom", positiv_liste=[example_regionskriterium]), id="min attributes"
-            ),
+            pytest.param(Region(bezeichnung="Bikini Bottom", positiv_liste=[Regionskriterium()]), id="min attributes"),
         ],
     )
     def test_serialization_roundtrip(self, region: Region) -> None:
+        """
+        Test de-/serialisation
+        """
         assert_serialization_roundtrip(region)
-
-    def test_region_id(self) -> None:
-        region = Region(
-            bezeichnung="Bikini Bottom",
-            id="foo",
-            positiv_liste=[example_regionskriterium],
-            negativ_liste=[example_regionskriterium],
-        )
-        region_dict = region.model_dump(by_alias=True)
-        assert "_id" in region_dict
-        assert Region.model_validate(region_dict) == region
