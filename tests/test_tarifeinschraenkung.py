79d3b397ab6427b9a7b7882bf296fe4e74e1ebb2
Stefan Heese <stefan.heese@hochfrequenz.de>
2023-11-08 15:38:45 +0100

    ✅Test Tarifeinschraenkung

--- tests/test_tarifeinschraenkung.py
+++ tests/test_tarifeinschraenkung.py
@@ -15,25 +15,6 @@ from bo4e import (
 )
 from tests.serialization_helper import assert_serialization_roundtrip
 
-example_tarifeinschraenkung = Tarifeinschraenkung(
-    zusatzprodukte=["foo", "bar"],
-    voraussetzungen=[Voraussetzungen.ALTVERTRAG, Voraussetzungen.DIREKTVERTRIEB],
-    einschraenkungzaehler=[
-        Geraet(
-            geraetenummer="0815",
-            geraeteeigenschaften=Geraeteeigenschaften(
-                geraetemerkmal=Geraetemerkmal.GAS_G1000,
-                geraetetyp=Geraetetyp.MULTIPLEXANLAGE,
-            ),
-        ),
-        Geraet(geraetenummer="197foo"),
-    ],
-    einschraenkungleistung=[
-        Menge(wert=Decimal(12.5), einheit=Mengeneinheit.MWH),
-        Menge(wert=Decimal(30), einheit=Mengeneinheit.KWH),
-    ],
-)
-
 
 class TestTarifeinschraenkung:
     @pytest.mark.parametrize(
