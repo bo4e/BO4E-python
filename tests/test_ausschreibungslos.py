e3648950330daa3535cf93864c6ce4562f6de26a
Stefan Heese <stefan.heese@hochfrequenz.de>
2023-11-09 10:23:02 +0100

    ✅Remove example_ausschreibungslos

--- tests/test_ausschreibungslos.py
+++ tests/test_ausschreibungslos.py
@@ -15,33 +15,31 @@ from bo4e import (
 )
 from tests.serialization_helper import assert_serialization_roundtrip
 
-example_ausschreibungslos = Ausschreibungslos(
-    losnummer="foo",
-    bezeichnung="bar",
-    bemerkung="asd",
-    preismodell=Preismodell.FESTPREIS,
-    energieart=Sparte.STROM,
-    wunsch_rechnungslegung=Rechnungslegung.MONATSRECHN,
-    wunsch_vertragsform=Vertragsform.DIREKT,
-    betreut_durch="Max Mustermann",
-    anzahl_lieferstellen=17,
-    lieferstellen=[Ausschreibungsdetail()],
-    gesamt_menge=Menge(),
-    wunsch_mindestmenge=Menge(),
-    wunsch_maximalmenge=Menge(),
-    lieferzeitraum=Zeitraum(),
-    wunsch_kuendingungsfrist=Zeitraum(),
-    wunsch_zahlungsziel=Zeitraum(),
-    wiederholungsintervall=Zeitraum(),
-)
-
 
 class TestAusschreibungslos:
     @pytest.mark.parametrize(
         "ausschreibungslos",
         [
             pytest.param(
-                example_ausschreibungslos,
+                Ausschreibungslos(
+                    losnummer="foo",
+                    bezeichnung="bar",
+                    bemerkung="asd",
+                    preismodell=Preismodell.FESTPREIS,
+                    energieart=Sparte.STROM,
+                    wunsch_rechnungslegung=Rechnungslegung.MONATSRECHN,
+                    wunsch_vertragsform=Vertragsform.DIREKT,
+                    betreut_durch="Max Mustermann",
+                    anzahl_lieferstellen=17,
+                    lieferstellen=[Ausschreibungsdetail()],
+                    gesamt_menge=Menge(),
+                    wunsch_mindestmenge=Menge(),
+                    wunsch_maximalmenge=Menge(),
+                    lieferzeitraum=Zeitraum(),
+                    wunsch_kuendingungsfrist=Zeitraum(),
+                    wunsch_zahlungsziel=Zeitraum(),
+                    wiederholungsintervall=Zeitraum(),
+                ),
                 id="maximal attributes",
             ),
         ],
