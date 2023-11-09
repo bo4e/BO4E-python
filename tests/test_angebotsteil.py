2ebff073cc6cb2f9cbe8c8e8771b11f4a38ae53f
Stefan Heese <stefan.heese@hochfrequenz.de>
2023-11-09 10:03:37 +0100

    ✅Remove example_angebotsteil, example_angebotsteil_json

--- tests/test_angebotsteil.py
+++ tests/test_angebotsteil.py
@@ -5,64 +5,8 @@ from typing import Any, Dict
 import pytest
 from pydantic import ValidationError
 
-from bo4e import (
-    Adresse,
-    Angebotsposition,
-    Angebotsteil,
-    Betrag,
-    Bilanzierungsmethode,
-    Energierichtung,
-    Landescode,
-    Marktlokation,
-    Menge,
-    Mengeneinheit,
-    Netzebene,
-    Preis,
-    Sparte,
-    Typ,
-    Waehrungscode,
-    Waehrungseinheit,
-    Zeitraum,
-)
+from bo4e import Angebotsposition, Angebotsteil, Betrag, Marktlokation, Menge, Zeitraum
 from tests.serialization_helper import assert_serialization_roundtrip
-
-example_angebotsteil: Angebotsteil = Angebotsteil(
-    positionen=[
-        Angebotsposition(
-            positionsbezeichnung="teststring",
-            positionsmenge=Menge(wert=Decimal(4000), einheit=Mengeneinheit.KWH),
-            positionspreis=Preis(wert=Decimal(0.2456), einheit=Waehrungseinheit.EUR, bezugswert=Mengeneinheit.KWH),
-            positionskosten=Betrag(
-                waehrung=Waehrungscode.EUR,
-                wert=Decimal(98240),
-            ),
-        )
-    ],
-)
-
-example_angebotsteil_json = {
-    "positionen": [
-        {
-            "positionsbezeichnung": "teststring",
-            "positionsmenge": {"wert": Decimal("4000"), "einheit": Mengeneinheit.KWH, "_id": None},
-            "positionskosten": {"waehrung": Waehrungseinheit.EUR, "wert": Decimal("98240"), "_id": None},
-            "positionspreis": {
-                "bezugswert": Mengeneinheit.KWH,
-                "status": None,
-                "wert": Decimal("0.2456000000000000127453603226967970840632915496826171875"),
-                "einheit": Waehrungseinheit.EUR,
-                "_id": None,
-            },
-            "_id": None,
-        },
-    ],
-    "anfrageSubreferenz": None,
-    "lieferstellenangebotsteil": None,
-    "gesamtmengeangebotsteil": None,
-    "gesamtkostenangebotsteil": None,
-    "lieferzeitraum": None,
-    "_id": None,
-}
 
 
 class TestAngebotsteil:
--- tests/test_angebotsvariante.py
+++ tests/test_angebotsvariante.py
@@ -5,18 +5,15 @@ from typing import Any, Dict
 import pytest
 from pydantic import ValidationError
 
-from bo4e import Angebotsstatus, Angebotsteil, Angebotsvariante, Betrag, Menge, Mengeneinheit
+from bo4e import Angebotsstatus, Angebotsteil, Angebotsvariante, Betrag, Menge
 from tests.serialization_helper import assert_serialization_roundtrip
-from tests.test_angebotsteil import example_angebotsteil, example_angebotsteil_json
-from tests.test_betrag import example_betrag
-from tests.test_menge import example_menge
 
 # can be imported by other tests
 example_angebotsvariante = Angebotsvariante(
     angebotsstatus=Angebotsstatus.NACHGEFASST,
     bindefrist=datetime(2022, 2, 1, 0, 0, 0, tzinfo=timezone.utc),
     erstellungsdatum=datetime(2021, 12, 22, 0, 0, 0, tzinfo=timezone.utc),
-    teile=[example_angebotsteil],
+    teile=[Angebotsteil()],
 )
 
 
