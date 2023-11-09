f1869cf2e9d3dea17c45eb5d666f4a879ea26dc9
Stefan Heese <stefan.heese@hochfrequenz.de>
2023-11-09 10:21:05 +0100

    ✅Remove example_ausschreibungsdetail

--- tests/test_ausschreibungsdetail.py
+++ tests/test_ausschreibungsdetail.py
@@ -4,65 +4,8 @@ from typing import Any, Dict
 import pytest
 from pydantic import ValidationError
 
-from bo4e import Adresse, Ausschreibungsdetail, Landescode, Menge, Netzebene, Zaehlertyp, Zeiteinheit, Zeitraum
+from bo4e import Adresse, Ausschreibungsdetail, Menge, Netzebene, Zaehlertyp, Zeitraum
 from tests.serialization_helper import assert_serialization_roundtrip
-
-example_ausschreibungsdetail = Ausschreibungsdetail(
-    marktlokations_id="56789012345",
-    netzebene_lieferung=Netzebene.MSP,
-    netzebene_messung=Netzebene.NSP,
-    marktlokationsadresse=Adresse(),
-    lieferzeitraum=Zeitraum(),
-    rechnungsadresse=Adresse(),
-)
-example_ausschreibungsdetail_dict = {
-    "zaehlernummer": None,
-    "zaehlertechnik": None,
-    "kunde": None,
-    "marktlokationsbezeichnung": None,
-    "lieferzeitraum": {
-        "dauer": Decimal("5"),
-        "startdatum": None,
-        "endzeitpunkt": None,
-        "enddatum": None,
-        "einheit": Zeiteinheit.TAG,
-        "startzeitpunkt": None,
-        "_id": None,
-    },
-    "marktlokationsadresse": {
-        "landescode": Landescode.DE,  # type: ignore[attr-defined]
-        "hausnummer": "27A",
-        "strasse": "Nördliche Münchner Straße",
-        "postleitzahl": "82031",
-        "ort": "Grünwald",
-        "adresszusatz": None,
-        "postfach": None,
-        "coErgaenzung": None,
-        "ortsteil": None,
-        "_id": None,
-    },
-    "rechnungsadresse": {
-        "landescode": Landescode.DE,  # type: ignore[attr-defined]
-        "hausnummer": "27A",
-        "strasse": "Nördliche Münchner Straße",
-        "postleitzahl": "82031",
-        "ort": "Grünwald",
-        "adresszusatz": None,
-        "postfach": None,
-        "coErgaenzung": None,
-        "ortsteil": None,
-        "_id": None,
-    },
-    "netzbetreiber": None,
-    "netzebeneLieferung": Netzebene.MSP,
-    "prognoseArbeitLieferzeitraum": None,
-    "netzebeneMessung": Netzebene.NSP,
-    "prognoseLeistung": None,
-    "istLastgangVorhanden": None,
-    "prognoseJahresarbeit": None,
-    "marktlokationsId": "56789012345",
-    "_id": None,
-}
 
 
 class TestAusschreibungsdetail:
