db5aa105871b92d81cf8d5ac1f550415644a84a9
Stefan Heese <stefan.heese@hochfrequenz.de>
2023-11-09 10:14:15 +0100

    ✅Test Ansprechpartner

--- tests/test_ansprechpartner.py
+++ tests/test_ansprechpartner.py
@@ -1,37 +1,33 @@
-from bo4e import (
-    Adresse,
-    Anrede,
-    Ansprechpartner,
-    Geschaeftspartner,
-    Geschaeftspartnerrolle,
-    Kontaktart,
-    Rufnummer,
-    Rufnummernart,
-    Themengebiet,
-    Titel,
-    Typ,
-    Zustaendigkeit,
-)
+import pytest
+
+from bo4e import Adresse, Anrede, Ansprechpartner, Geschaeftspartner, Rufnummer, Titel, Zustaendigkeit
 from tests.serialization_helper import assert_serialization_roundtrip
 
 
 class TestAnsprechpartner:
-    def test_serialization_roundtrip(self) -> None:
+    @pytest.mark.parametrize(
+        "ansprechpartner",
+        [
+            pytest.param(
+                Ansprechpartner(
+                    nachname="Müller-Schmidt",
+                    geschaeftspartner=Geschaeftspartner(),
+                    anrede=Anrede.EHELEUTE,
+                    individuelle_anrede="Künstler",
+                    titel=Titel.PROF_DR,
+                    vorname="Hans",
+                    e_mail_adresse="hans.müller@getrei.de",
+                    kommentar="does this thing work?",
+                    adresse=Adresse(),
+                    rufnummer=Rufnummer(),
+                    zustaendigkeit=Zustaendigkeit(),
+                ),
+                id="all attributes at first level",  # = min + menge and betrag
+            ),
+        ],
+    )
+    def test_serialization_roundtrip(self, ansprechpartner: Ansprechpartner) -> None:
         """
         Test Test de-/serialisation roundtrip of Ansprechpartner
         """
-        ansprechpartner = Ansprechpartner(
-            nachname="Müller-Schmidt",
-            geschaeftspartner=Geschaeftspartner(),
-            anrede=Anrede.EHELEUTE,
-            individuelle_anrede="Künstler",
-            titel=Titel.PROF_DR,
-            vorname="Hans",
-            e_mail_adresse="hans.müller@getrei.de",
-            kommentar="does this thing work?",
-            adresse=Adresse(),
-            rufnummer=Rufnummer(),
-            zustaendigkeit=Zustaendigkeit(),
-        )
-
         assert_serialization_roundtrip(ansprechpartner)
