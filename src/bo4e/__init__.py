fd4d6f47f541582f4b3c7737dcbfd9383b19eb7a
Franziska <franziska.vesely@hochfrequenz.de>
2023-11-24 20:50:05 +0100

    Add new BO, COMs and ENUMs to init.py

--- src/bo4e/__init__.py
+++ src/bo4e/__init__.py
@@ -11,6 +11,7 @@ __all__ = [
     "Angebot",
     "Ansprechpartner",
     "Ausschreibung",
+    "Bilanzierung",
     "Buendelvertrag",
     "Energiemenge",
     "Fremdkosten",
@@ -63,6 +64,7 @@ __all__ = [
     "Kostenblock",
     "Kostenposition",
     "KriteriumWert",
+    "Lastprofil",
     "MarktgebietInfo",
     "Menge",
     "Messlokationszuordnung",
@@ -83,6 +85,7 @@ __all__ = [
     "StandorteigenschaftenGas",
     "StandorteigenschaftenStrom",
     "Steuerbetrag",
+    "Tagesparameter",
     "Tarifberechnungsparameter",
     "Tarifeinschraenkung",
     "Tarifpreis",
@@ -101,6 +104,7 @@ __all__ = [
     "Zeitspanne",
     "Zustaendigkeit",
     "AbgabeArt",
+    "Aggregationsverantwortung",
     "Angebotsstatus",
     "Anrede",
     "ArithmetischeOperation",
@@ -117,6 +121,7 @@ __all__ = [
     "Dienstleistungstyp",
     "Energierichtung",
     "Erzeugungsart",
+    "Fallgruppenzuordnung",
     "Gasqualitaet",
     "Gebiettyp",
     "Geraeteklasse",
@@ -150,6 +155,10 @@ __all__ = [
     "Preismodell",
     "Preisstatus",
     "Preistyp",
+    "Profilart",
+    "Profiltyp",
+    "Profilverfahren",
+    "Prognosegrundlage",
     "Rechnungslegung",
     "Rechnungsstatus",
     "Rechnungstyp",
@@ -174,6 +183,7 @@ __all__ = [
     "Vertragsstatus",
     "Verwendungszweck",
     "Voraussetzungen",
+    "WahlrechtPrognosegrundlage",
     "Waehrungscode",
     "Waehrungseinheit",
     "Waermenutzung",
@@ -182,6 +192,7 @@ __all__ = [
     "Zaehlergroesse",
     "Zaehlertyp",
     "ZaehlertypSpezifikation",
+    "Zeitreihentyp",
     "__version__",
 ]
 
@@ -189,6 +200,7 @@ __all__ = [
 from .bo.angebot import Angebot
 from .bo.ansprechpartner import Ansprechpartner
 from .bo.ausschreibung import Ausschreibung
+from .bo.bilanzierung import Bilanzierung
 from .bo.buendelvertrag import Buendelvertrag
 from .bo.energiemenge import Energiemenge
 from .bo.fremdkosten import Fremdkosten
@@ -243,6 +255,7 @@ from .com.konzessionsabgabe import Konzessionsabgabe
 from .com.kostenblock import Kostenblock
 from .com.kostenposition import Kostenposition
 from .com.kriteriumwert import KriteriumWert
+from .com.lastprofil import Lastprofil
 from .com.marktgebietinfo import MarktgebietInfo
 from .com.menge import Menge
 from .com.messlokationszuordnung import Messlokationszuordnung
@@ -263,6 +276,7 @@ from .com.sigmoidparameter import Sigmoidparameter
 from .com.standorteigenschaftengas import StandorteigenschaftenGas
 from .com.standorteigenschaftenstrom import StandorteigenschaftenStrom
 from .com.steuerbetrag import Steuerbetrag
+from .com.tagesparameter import Tagesparameter
 from .com.tarifberechnungsparameter import Tarifberechnungsparameter
 from .com.tarifeinschraenkung import Tarifeinschraenkung
 from .com.tarifpreis import Tarifpreis
@@ -283,6 +297,7 @@ from .com.zustaendigkeit import Zustaendigkeit
 
 # Import Enums
 from .enum.abgabeart import AbgabeArt
+from .enum.aggregationsverantwortung import Aggregationsverantwortung
 from .enum.angebotsstatus import Angebotsstatus
 from .enum.anrede import Anrede
 from .enum.arithmetische_operation import ArithmetischeOperation
@@ -299,6 +314,7 @@ from .enum.bilanzierungsmethode import Bilanzierungsmethode
 from .enum.dienstleistungstyp import Dienstleistungstyp
 from .enum.energierichtung import Energierichtung
 from .enum.erzeugungsart import Erzeugungsart
+from .enum.fallgruppenzuordnung import Fallgruppenzuordnung
 from .enum.gasqualitaet import Gasqualitaet
 from .enum.gebiettyp import Gebiettyp
 from .enum.geraeteklasse import Geraeteklasse
@@ -332,6 +348,10 @@ from .enum.preisgarantietyp import Preisgarantietyp
 from .enum.preismodell import Preismodell
 from .enum.preisstatus import Preisstatus
 from .enum.preistyp import Preistyp
+from .enum.profilart import Profilart
+from .enum.profiltyp import Profiltyp
+from .enum.profilverfahren import Profilverfahren
+from .enum.prognosegrundlage import Prognosegrundlage
 from .enum.rechnungslegung import Rechnungslegung
 from .enum.rechnungsstatus import Rechnungsstatus
 from .enum.rechnungstyp import Rechnungstyp
@@ -359,9 +379,11 @@ from .enum.voraussetzungen import Voraussetzungen
 from .enum.waehrungscode import Waehrungscode
 from .enum.waehrungseinheit import Waehrungseinheit
 from .enum.waermenutzung import Waermenutzung
+from .enum.wahlrechtprognosegrundlage import WahlrechtPrognosegrundlage
 from .enum.wertermittlungsverfahren import Wertermittlungsverfahren
 from .enum.zaehlerauspraegung import Zaehlerauspraegung
 from .enum.zaehlergroesse import Zaehlergroesse
 from .enum.zaehlertyp import Zaehlertyp
 from .enum.zaehlertypspezifikation import ZaehlertypSpezifikation
+from .enum.zeitreihentyp import Zeitreihentyp
 from .version import __version__
