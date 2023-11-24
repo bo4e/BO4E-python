fd6839beb334d5a073d104ed86cd58f76308618f
Franziska <73471037+hf-fvesely@users.noreply.github.com>
2023-11-24 18:12:58 +0100

    Update src/bo4e/com/zeitspanne.py

--- src/bo4e/com/zeitspanne.py
+++ src/bo4e/com/zeitspanne.py
@@ -10,7 +10,7 @@ from bo4e.com.com import COM
 class Zeitspanne(COM):
     """
     Eine Zeitspanne ist definiert aus Start und/oder Ende.
-    Der Unterschied zur Zeitmenge ist, dass konkrete Start- und Endzeitpunkte angegeben werden.
+    Der Unterschied zur Menge (die auch zur Abbildung von Zeitmengen genutzt wird) ist, dass konkrete Start- und Endzeitpunkte angegeben werden.
     Die Zeitspanne ist aus dem COM Zeitraum hervorgegangen, das in Zeitspanne und Zeitmenge aufgeteilt wurde.
     """
 
