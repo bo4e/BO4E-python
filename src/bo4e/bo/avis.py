05ae7b85ffae02d3588fe81dc70cb209dcbbf3f4
Franziska <franziska.vesely@hochfrequenz.de>
2023-11-24 21:46:36 +0100

    Change from BOTyp to Typ

--- src/bo4e/bo/avis.py
+++ src/bo4e/bo/avis.py
@@ -1,17 +1,21 @@
 """Contains class Avis"""
+from typing import Annotated, Optional
+
+from pydantic import Field
+
 from bo4e.bo.geschaeftsobjekt import Geschaeftsobjekt
 from bo4e.com.avisposition import Avisposition
 from bo4e.com.betrag import Betrag
 from bo4e.enum.avistyp import AvisTyp
-from bo4e.enum.botyp import BoTyp
+
+from ..enum.typ import Typ
 
 
 class Avis(Geschaeftsobjekt):
     """Avis BO"""
 
-    bo_typ: BoTyp = BoTyp.AVIS  # added to botyp.py
+    typ: Annotated[Optional[Typ], Field(alias="_typ")] = Typ.AVIS
 
-    #: required attributes
     avis_nummer: str  #: Eine im Verwendungskontext eindeutige Nummer für das Avis.
     avis_typ: AvisTyp  #: Gibt den Typ des Avis an.
     positionen: list[Avisposition]  #: Avispositionen
