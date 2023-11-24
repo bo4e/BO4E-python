3af3b6ff7f947070bd117c2c63792b557f20e48d
Franziska <franziska.vesely@hochfrequenz.de>
2023-11-24 18:57:11 +0100

    Forgot to save file

--- src/bo4e/__init__.py
+++ src/bo4e/__init__.py
@@ -96,7 +96,6 @@ __all__ = [
     "VerwendungszweckProMarktrolle",
     "Zaehlwerk",
     "Zaehlzeitregister",
-    "Zeitintervall",
     "Zeitraum",
     "Zeitreihenwert",
     "Zeitspanne",
@@ -277,7 +276,6 @@ from .com.vertragsteil import Vertragsteil
 from .com.verwendungszweckpromarktrolle import VerwendungszweckProMarktrolle
 from .com.zaehlwerk import Zaehlwerk
 from .com.zaehlzeitregister import Zaehlzeitregister
-from .com.zeitintervall import Zeitintervall
 from .com.zeitraum import Zeitraum
 from .com.zeitreihenwert import Zeitreihenwert
 from .com.zeitspanne import Zeitspanne
