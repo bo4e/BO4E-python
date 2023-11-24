e20961e529e8e105196571c339d85c10388ab1ca
Franziska <franziska.vesely@hochfrequenz.de>
2023-11-24 21:17:18 +0100

    And also Tagesparameter test

--- tests/test_tagesparameter.py
+++ tests/test_tagesparameter.py
@@ -1,48 +1,25 @@
-from typing import Any, Dict
-
 import pytest
 
 from bo4e.com.tagesparameter import Tagesparameter
 from tests.serialization_helper import assert_serialization_roundtrip
 
-# full example
-example_tagesparameter = Tagesparameter(
-    klimazone="7624q",
-    temperaturmessstelle="1234x",
-    dienstanbieter="ZT1",
-    herausgeber="BDEW",
-)
-
 
 class TestTagesparameter:
-    #
     @pytest.mark.parametrize(
-        "tagesparameter, expected_json_dict",
+        "tagesparameter",
         [
             pytest.param(
-                example_tagesparameter,
-                {
-                    "klimazone": "7624q",
-                    "temperaturmessstelle": "1234x",
-                    "dienstanbieter": "ZT1",
-                    "herausgeber": "BDEW",
-                },
-                id="full example",
-            ),
-            pytest.param(
-                Tagesparameter(),
-                {
-                    "klimazone": None,
-                    "temperaturmessstelle": None,
-                    "dienstanbieter": None,
-                    "herausgeber": None,
-                },
-                id="min example",
+                Tagesparameter(
+                    klimazone="7624q",
+                    temperaturmessstelle="1234x",
+                    dienstanbieter="ZT1",
+                    herausgeber="BDEW",
+                )
             ),
         ],
     )
-    def test_serialization_roundtrip(self, tagesparameter: Tagesparameter, expected_json_dict: Dict[str, Any]) -> None:
+    def test_serialization_roundtrip(self, tagesparameter: Tagesparameter) -> None:
         """
-        Test de-/serialisation of Tagesparameter with maximal/minimal attributes.
+        Test de-/serialisation of Tagesparameter.
         """
-        assert_serialization_roundtrip(tagesparameter, expected_json_dict)
+        assert_serialization_roundtrip(tagesparameter)
