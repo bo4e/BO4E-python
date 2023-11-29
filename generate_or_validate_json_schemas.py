3e70fb6fbbfdba03605dded41c721f99086e84e8
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2023-11-29 23:10:13 +0100

    JSON-Schema Release Workflow

---
+++ generate_or_validate_json_schemas.py
@@ -0,0 +1,132 @@
+"""
+This script is run in the tox 'json_schemas' environment.
+"""
+
+import importlib
+import inspect
+import json
+import logging
+import pathlib
+import pkgutil
+import sys
+from pathlib import Path
+from typing import Any, Iterator, Literal, cast
+
+import click
+
+logging.basicConfig(level=logging.NOTSET, stream=sys.stdout)
+_logger = logging.getLogger(__name__)
+root_directory = Path(__file__).parent
+output_directory = root_directory / "json_schemas"
+
+
+def delete_json_schemas(packages: list[str]) -> None:
+    """delete all json schemas"""
+    for pkg in packages:
+        to_delete = output_directory / pkg
+        if to_delete.exists():
+            for file in to_delete.iterdir():
+                file.unlink()
+
+
+def get_models(pkg: str) -> Iterator[str]:
+    """
+    Get all models in a package
+    """
+    yield from (name for _, name, _ in pkgutil.iter_modules([str(root_directory / "src" / "bo4e" / pkg)]))
+
+
+def get_classes(modl_name: str) -> list[tuple[str, type]]:
+    """
+    Get all classes in a module
+    """
+    modl = importlib.import_module(modl_name)
+    return inspect.getmembers(modl, lambda member: inspect.isclass(member) and member.__module__ == modl_name)
+
+
+def get_schema_json_dict(cls: type[BaseModel]) -> dict[str, Any]:
+    """
+    Get the json schema for a class
+    """
+    schema_json_dict = cls.model_json_schema()
+    if "definitions" in schema_json_dict:
+        for definition in schema_json_dict["definitions"].values():
+            definition["description"] = definition["description"].strip()
+    return schema_json_dict
+
+
+def validate_schema(file_path: Path, schema_json_dict: dict[str, Any], name: str) -> None:
+    """
+    Validate the schema for a class
+    """
+    with open(file_path, "r", encoding="utf-8") as json_schema_file:
+        existing_schema = json.load(json_schema_file)
+
+    if schema_json_dict != existing_schema:
+        raise ValueError(f"Schema for {name} has changed. Please run this script with mode 'generate'.")
+        # or call tox -e generate_json_schemas
+    _logger.debug("Schema for %s is consistent", name)
+
+
+def generate_schema(file_path: Path, schema_json_dict: dict[str, Any], name: str) -> None:
+    """
+    Generate the schema for a class
+    """
+    if not file_path.parent.exists():
+        file_path.parent.mkdir(parents=True)
+    with open(file_path, "w+", encoding="utf-8") as json_schema_file:
+        json_schema_file.write(json.dumps(schema_json_dict, indent=4, sort_keys=True, ensure_ascii=False))
+        json_schema_file.write("\n")
+    _logger.debug("Generated schema for %s", name)
+
+
+@click.command()
+@click.option(
+    "--mode",
+    "-m",
+    help="use 'validate' to validate existing schemas or 'generate' to generate new schemas",
+    required=True,
+    type=click.Choice(["validate", "generate"]),
+)
+def generate_or_validate_json_schemas(mode: Literal["validate", "generate"]) -> None:
+    """generate json schemas for all BOs and COMs"""
+    packages = ["bo", "com"]
+
+    if mode == "generate":
+        delete_json_schemas(packages)
+
+    for pkg in packages:
+        for model in get_models(pkg):
+            modl_name = f"bo4e.{pkg}.{model}"
+            modl = importlib.import_module(modl_name)
+            # pylint: disable=cell-var-from-loop
+            cls_list = inspect.getmembers(
+                modl, lambda member: inspect.isclass(member) and member.__module__ == modl_name
+            )
+            this_directory = pathlib.Path(__file__).parent.absolute()
+            for name, cls in cls_list:
+                _logger.info("Processing %s", name)
+                file_path = output_directory / pkg / (name + ".json")
+
+                schema_json_dict = get_schema_json_dict(cast(type[BaseModel], cls))
+
+                if mode == "validate":
+                    with open(file_path, "r", encoding="utf-8") as json_schema_file:
+                        existing_schema = json.load(json_schema_file)
+                    if schema_json_dict != existing_schema:
+                        raise ValueError(f"Schema for {name} has changed. Please run this script with mode 'generate'.")
+                        # or call tox -e generate_json_schemas
+                    _logger.debug("Schema for %s is consistent", name)
+                elif mode == "generate":
+                    with open(file_path, "w+", encoding="utf-8") as json_schema_file:
+                        json_schema_file.write(
+                            json.dumps(schema_json_dict, indent=4, sort_keys=True, ensure_ascii=False)
+                        )
+                        json_schema_file.write("\n")  # empty line at EOF for those with broken pre-commit hook ;)
+                    _logger.debug("Generated schema for %s", name)
+                else:
+                    raise ValueError(f"Unknown mode '{mode}'")
+
+
+if __name__ == "__main__":
+    generate_or_validate_json_schemas()  # pylint:disable=no-value-for-parameter
