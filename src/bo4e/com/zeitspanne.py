5ace9d098e2318a7c058ed9c5ff7796b720641d5
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2023-12-05 23:48:06 +0100

    🩹Fix Zeitspanne docstring

--- src/bo4e/com/zeitspanne.py
+++ src/bo4e/com/zeitspanne.py
@@ -21,7 +21,7 @@ class Zeitspanne(COM):
         <object data="../_static/images/bo4e/com/Zeitspanne.svg" type="image/svg+xml"></object>
 
     .. HINT::
-        `Zeitreihenwert JSON Schema <https://json-schema.app/view/%23?url=https://raw.githubusercontent.com/Hochfrequenz/BO4E-Schemas/{__gh_version__}/src/bo4e_schemas/com/Zeitspanne.json>`_
+        `Zeitspanne JSON Schema <https://json-schema.app/view/%23?url=https://raw.githubusercontent.com/Hochfrequenz/BO4E-Schemas/{__gh_version__}/src/bo4e_schemas/com/Zeitspanne.json>`_
 
     """
 
