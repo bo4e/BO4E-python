851ac625e50b9a6afcca5bc6ba4b3ecce3a9b68c
hf-krechan <kevin.krechan@hochfrequenz.de>
2023-12-22 16:41:47 +0100

    try and except for version

--- src/bo4e/version.py
+++ src/bo4e/version.py
@@ -4,7 +4,10 @@ Version information for the bo4e package.
 import re
 from importlib.metadata import version
 
-__version__ = version("bo4e")
+try:
+    __version__ = version("bo4e")
+except Exception:
+    __version__ = "0.0.0"
 # Please keep this name in sync with the name of the project in pyproject.toml
 # This name is the name of the package on pypi.org
 if re.match(r"^(\d+\.\d+\.\d+)(rc\d+)?$", __version__):
