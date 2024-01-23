9b77d529fccf56ae35eed12a0784cfdb2cf81557
hf-krechan <kevin.krechan@hochfrequenz.de>
2024-01-23 11:52:59 +0100

    📝 add information about 1 and 3 phases

--- src/bo4e/enum/zaehlertyp.py
+++ src/bo4e/enum/zaehlertyp.py
@@ -8,14 +8,18 @@ class Zaehlertyp(StrEnum):
     Bei diesem Enum handelt es sich um die Abbildung von Zählertypen der Sparten Strom und Gas.
     """
 
-    DREHSTROMZAEHLER = "DREHSTROMZAEHLER"  #: Drehstromzähler - dieser Zählertyp wird für Ferraris-Zähler verwendet
+    DREHSTROMZAEHLER = (
+        "DREHSTROMZAEHLER"  #: Drehstromzähler - dieser Zählertyp wird für dreiphasige Ferraris-Zähler verwendet
+    )
     BALGENGASZAEHLER = "BALGENGASZAEHLER"  #: Balgengaszähler
     DREHKOLBENZAEHLER = "DREHKOLBENZAEHLER"  #: Drehkolbengaszähler
     LEISTUNGSZAEHLER = "LEISTUNGSZAEHLER"  #: leistungsmessender Zähler
     MAXIMUMZAEHLER = "MAXIMUMZAEHLER"  #: Maximumzähler
     TURBINENRADGASZAEHLER = "TURBINENRADGASZAEHLER"  #: Turbinenradgaszähler
     ULTRASCHALLGASZAEHLER = "ULTRASCHALLGASZAEHLER"  #: Ultraschallgaszähler
-    WECHSELSTROMZAEHLER = "WECHSELSTROMZAEHLER"  #: Wechselstromzähler
+    WECHSELSTROMZAEHLER = (
+        "WECHSELSTROMZAEHLER"  #: Wechselstromzähler - dieser Zählertyp wird für einphasige Ferraris-Zähler verwendet
+    )
     MODERNE_MESSEINRICHTUNG = "MODERNE_MESSEINRICHTUNG"  #: Moderne Messeinrichtung
     INTELLIGENTES_MESSSYSTEM = "INTELLIGENTES_MESSSYSTEM"  #: Intelligentes Messsystem
     ELEKTRONISCHER_ZAEHLER = "ELEKTRONISCHER_ZAEHLER"  #: Elektronischer Zähler
