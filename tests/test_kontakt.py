05faaed041f02008b62d3fd664bb3c429239d135
Franziska <franziska.vesely@hochfrequenz.de>
2024-02-07 17:17:07 +0100

    Fix test

--- tests/test_kontakt.py
+++ tests/test_kontakt.py
@@ -1,6 +1,6 @@
 import pytest
 
-from bo4e import Adresse, Kontaktart, Kontaktweg
+from bo4e import Kontaktart, Kontaktweg
 from tests.serialization_helper import assert_serialization_roundtrip
 
 
@@ -11,8 +11,8 @@ class TestKontakt:
             pytest.param(
                 Kontaktweg(
                     kontaktart=Kontaktart.E_MAIL,
-                    e_mail_adresse="test@bo4e.de",
-                    adresse=Adresse(),
+                    beschreibung="irgendeine Spezifikation",
+                    kontaktwert="test@bo4e.de",
                 ),
                 id="all attributes at first level",
             ),
