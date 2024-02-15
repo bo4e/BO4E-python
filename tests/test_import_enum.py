b497e4690331b13b126e32e7c66c7bab96eee61e
konstantin <konstantin.klein@hochfrequenz.de>
2024-02-15 13:19:46 +0100

    test: add check for enum import

---
+++ tests/test_import_enum.py
@@ -0,0 +1,7 @@
+class TestImport:
+    def test_importing_an_enum(self) -> None:
+        """
+        checks that importing an enum works
+        """
+        from bo4e import Vertragsstatus # pylint: disable=import-outside-toplevel,unused-import
+        assert True
