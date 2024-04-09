231588ad4b3eaf266f3dbcedddf1784f9c0ad5e0
Lev <lev.perschin@gmail.com>
2024-04-09 12:12:24 +0200

    feat: add new lokationszuordnung

---
+++ src/bo4e/bo/lokationszuordnung.py
@@ -0,0 +1,33 @@
+from typing import Annotated, Optional
+
+from pydantic import Field
+
+from .geschaeftsobjekt import Geschaeftsobjekt
+from ..enum.typ import Typ
+from ..com.zeitspanne import Zeitspanne
+from ..enum.arithmetische_operation import ArithmetischeOperation
+
+class Lokationszuordnung(Geschaeftsobjekt):
+    """
+    Modell für die Abbildung der Referenz auf die Lokationsbündelstruktur. Diese gibt an welche Marktlokationen,
+    Messlokationen, Netzlokationen, technische/steuerbaren Ressourcen an einer Lokation vorhanden sind;
+    """
+
+    typ: Annotated[Optional[Typ], Field(alias="_typ")] = Typ.LOKATIONSZUORDNUNG
+
+    #: Liste mit IDs der referenzierten Marktlokationen
+    marktlokationen: Optional[list[str]] = None
+    #: Liste mit IDs der referenzierten Messlokationen
+    messlokationen: Optional[list[str]] = None
+    #: Liste mit IDs der referenzierten Netzlokationen
+    netzlokationen: Optional[list[str]] = None
+    #: Liste mit IDs der referenzierten technischen Ressourcen
+    technische_ressourcen: Optional[list[str]] = None
+    #: Liste mit IDs der referenzierten steuerbaren Ressourcen
+    steuerbare_ressourcen: Optional[list[str]] = None
+    #: Zeitspanne der Gültigkeit
+    gueltigkeit: Optional[Zeitspanne] = None
+    #: Angabe einer arithmetischen Operation
+    arithmetik: Optional[ArithmetischeOperation] = None
+    #: Code, der angibt wie die Lokationsbündelstruktur zusammengesetzt ist
+    zuordnungstyp: Optional[str] = None
