0c4b16f29b7921974ba72a15eb788a7a6e1ea37e
kevin <kevin.krechan@hochfrequenz.de>
2025-03-20 10:38:35 +0100

    add more test to test_zeitraum

--- tests/test_zeitraum.py
+++ tests/test_zeitraum.py
@@ -1,8 +1,8 @@
-from decimal import Decimal
+from datetime import date, time, timedelta, timezone
 
 import pytest
 
-from bo4e import Mengeneinheit, Zeitraum
+from bo4e import Zeitraum
 from tests.serialization_helper import assert_serialization_roundtrip
 
 
@@ -12,9 +12,37 @@ class TestZeitraum:
         [
             pytest.param(
                 Zeitraum(
-                    einheit=Mengeneinheit.TAG,
-                    dauer=Decimal(21),
+                    dauer="P1DT30H4S",
                 ),
+                id="P1DT30H4S",
+            ),
+            pytest.param(
+                Zeitraum(
+                    startdatum=date(2025, 1, 1),
+                    enddatum=date(2025, 1, 31),
+                ),
+                id="startdatum-enddatum",
+            ),
+            pytest.param(
+                Zeitraum(
+                    startuhrzeit=time(10, 0, 0),
+                    enduhrzeit=time(11, 0, 0),
+                ),
+                id="startuhrzeit-enduhrzeit",
+            ),
+            pytest.param(
+                Zeitraum(
+                    startuhrzeit=time(10, 0, 0, tzinfo=timezone.utc),
+                    enduhrzeit=time(11, 0, 0, tzinfo=timezone.utc),
+                ),
+                id="startuhrzeit-enduhrzeit-utc-timezone",
+            ),
+            pytest.param(
+                Zeitraum(
+                    startuhrzeit=time(18, 0, 0, tzinfo=timezone(timedelta(hours=1), "Europe/Berlin")),
+                    enduhrzeit=time(19, 0, 0, tzinfo=timezone(timedelta(hours=1), "Europe/Berlin")),
+                ),
+                id="startuhrzeit-enduhrzeit-utc-timezone",
             ),
         ],
     )
