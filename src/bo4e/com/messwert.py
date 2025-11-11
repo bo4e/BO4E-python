8d042f5ca88c1127c5c3d509567ccab403b6eaaa
Simon Blank <simon.blank@enet.eu>
2025-11-11 10:52:42 +0100

    #994 Added Docs

--- src/bo4e/com/messwert.py
+++ src/bo4e/com/messwert.py
@@ -18,16 +18,16 @@ if TYPE_CHECKING:
 @postprocess_docstring
 class Messwert(COM):
     """
-    Abbildung eines Messwertes mit Status, Zeitpunkt und Wert.
+    Abbildung eines Messwertes mit Stati, Zeitpunkt und Wert.
     """
 
     typ: Annotated[Literal[ComTyp.MESSWERT], Field(alias="_typ")] = ComTyp.MESSWERT
 
     messwertstatus: Optional["Messwertstatus"] = None
-    """"""
+    """Gibt den Status des Messwerts an."""
     messwertstatuszusatz: Optional["Messwertstatuszusatz"] = None
-    """"""
+    """Gibt den Status Zusatz des Messwerts an."""
     zeitpunkt: Optional[pydantic.AwareDatetime] = None
-    """"""
+    """Gibt den Zeitpunkt des Messwerts an."""
     wert: Optional["Menge"] = None
-    """"""
+    """Gibt die gemessene Menge an."""
