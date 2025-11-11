748dff7d9758dde94e993f5e4e2bfb6b23ec4905
Simon Blank <simon.blank@enet.eu>
2025-11-11 11:30:40 +0100

    #994 added Messwert to __init__.py

--- src/bo4e/__init__.py
+++ src/bo4e/__init__.py
@@ -149,6 +149,7 @@ __all__ = [
     "Messart",
     "Messgroesse",
     "Messpreistyp",
+    "Messwert",
     "Messwerterfassung",
     "Messwertstatus",
     "Messwertstatuszusatz",
@@ -277,6 +278,7 @@ from .com.kriteriumwert import KriteriumWert
 from .com.lastprofil import Lastprofil
 from .com.marktgebietinfo import MarktgebietInfo
 from .com.menge import Menge
+from .com.messwert import Messwert
 from .com.positionsaufabschlag import PositionsAufAbschlag
 from .com.preis import Preis
 from .com.preisgarantie import Preisgarantie
