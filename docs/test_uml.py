9c1882c3739cea103ba15b790cd860de48b09efb
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2025-11-26 15:50:04 +0100

    Add little function to detect unconnected COMs and ENUMs

--- docs/test_uml.py
+++ docs/test_uml.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from docs.uml import PlantUMLNetwork, build_network
+from docs.uml import PlantUMLNetwork, build_network, get_unconnected_coms_and_enums
 
 
 def test_network_build() -> None:
@@ -10,3 +10,5 @@ def test_network_build() -> None:
     project_root_dir = Path(__file__).parent.parent
     module_dir = project_root_dir / "src/bo4e"
     _network, _namespaces_to_parse = build_network(module_dir, PlantUMLNetwork)
+    unconnected_els = get_unconnected_coms_and_enums(_network)
+    print(unconnected_els)
